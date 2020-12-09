4e3cbd7f5653f00e21ae9a0c4c2a34cba8a5f1fd
Daniel Gonzalez <3306466721202@ingenieria.usac.edu.gt>
2020-12-08 21:47:10 -0600

    Implementación de clase Nodo, y las clases necesarias para el uso de los tipo de datos y el almacenamiento de los atributos

---
+++ parser/team12/src/ENTORNO/Tipo.py
@@ -0,0 +1,52 @@
+import enum
+
+# *********************************************** Clase Enum ***********************************************
+#Enum para los tipos de datos
+class Data_Type(enum.Enum):
+    numeric = 1
+    character = 2
+    data_time = 3
+    boolean = 4
+    enumerated = 5
+# **********************************************************************************************************
+# *********************************************** Clase Tipo ***********************************************
+class Type():
+
+    # Constructor
+    def __init__(self, data_type, size):
+        self.data_type = data_type
+        self.size = size
+    
+    # Metodos para verificar el tipo de dato
+    def is_numeric(self):
+        return self.data_type == Data_Type.numeric
+
+    def is_character(self):
+        return self.data_type == Data_Type.character
+    
+    def is_data_time(self):
+        return self.data_type == Data_Type.data_time
+
+    def is_boolean(self):
+        return self.data_type == Data_Type.boolean
+    
+    def is_enumerated(self):
+        return self.data_type == Data_Type.enumerated
+
+    # Verificar que sea del mismo tipo
+    def mismoTipo(self, tipo):
+
+        if self.is_numeric() and tipo.is_numeric():
+            return True
+        elif self.is_character() and tipo.is_character():
+            return True
+        elif self.is_data_time() and tipo.is_data_time():
+            return True
+        elif self.is_boolean() and tipo.is_boolean():
+            return True
+        elif self.is_enumerated() and tipo.is_enumerated():
+            return True
+        else:
+            return False
+
+# **********************************************************************************************************
