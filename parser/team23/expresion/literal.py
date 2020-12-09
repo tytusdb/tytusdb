e6ffbd65c904a197c3bcdae98f6897b0e6472134
Luis Arana <luarana631@gmail.com>
2020-12-08 23:10:42 -0600

    Grupo 23 - 201700988 - Borrar archivos basura, agregar funciones create

--- parser/team23/expresion/literal.py
+++ parser/team23/expresion/literal.py
@@ -1,6 +1,5 @@
 from abstract.expresion import *
 from tools.tabla_tipos import *
-from abstract.retorno import *
 
 class literal(expresion):
     def __init__(self, line, column, valor, tipo, num_nodo):
@@ -11,8 +10,5 @@ class literal(expresion):
         #Nodo AST Literal
         self.nodo = nodo_AST(valor, num_nodo)
 
-    def ejecutar(self, ambiente):
-        if self.tipo == tipo_primitivo.DECIMAL or self.tipo == tipo_primitivo.ENTERO:
-            return retorno(int(self.valor), self.tipo)
-        else:
-            return retorno(self.valor, self.tipo)
+    def ejecutar(self):
+        pass
