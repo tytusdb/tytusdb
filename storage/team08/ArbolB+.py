5bfd6295d5bcc8d18f41618f0c4d052a356c5aff
Alexander Guerra <3018766650101@ingenieria.usac.edu.gt>
2020-12-10 11:57:57 -0600

    Insertar clave en nodo hoja
    Se crea el metodo insertar y agrega la clave en el nodo hoja correspondiente.

--- storage/team08/ArbolB+.py
+++ storage/team08/ArbolB+.py
@@ -9,10 +9,22 @@ class Nodo:
         self.claves = []
         self.hijos = []
         self.padre = None
-        
+
+    def nuevoNodo(self, claves, hijos, padre):
+        self.claves = claves
+        self.hijos = hijos
+        self.padre = padre
+        self.cuenta = len(claves)
+    
+    def addHijo(self, hijo):
+        self.hijos.append(hijo)
+        self.hijos = sorted(self.hijos ,key=lambda x: x.claves)
 
     def nodoLLeno(self):
         return self.cuenta >= self.orden
+
+    def esHoja(self):
+        return len(self.hijos)==0
 
     def nodoSemiVacio(self):
         return self.cuenta <= self.orden/2
@@ -25,7 +37,7 @@ class Nodo:
     def buscar(self, clave):
         if len(self.hijos)==0:
             return self
-        return self.__buscar(0 ,clave)
+        return self.__buscar(0 , clave)
 
     def __buscar(self, valor, clave):
         if valor == len(self.claves):
@@ -36,7 +48,17 @@ class Nodo:
 
         return self.hijos[valor].buscar(clave)
 
+    def buscarHijo(self, clave):
+        return self.__buscarHijo(0, clave)
+    
+    def __buscarHijo(self, valor,clave):
+        if valor == len(self.claves):
+            return len(self.hijos)-1
 
+        if clave > self.claves[valor]:
+            return self.__buscarHijo(valor+1,clave)
+
+        return valor
 
 class Arbol:
 
@@ -45,6 +67,47 @@ class Arbol:
         self.raiz = None
         self.gr = None
 
+#*************** INSERTAR **********************************************************
+    def insertar(self, clave):
+        self.raiz = self.__insertar(self.raiz, clave)
+
+    def __insertar(self, raiz, clave):
+        if self.raiz == None:
+            raiz = Nodo(self.orden)
+            raiz.nuevoNodo([clave], [], None)
+            return raiz
+        else:
+            n = raiz.buscarHijo(clave)
+            if raiz.esHoja():
+                raiz.addClave(clave)
+            else:
+                raiz = raiz.hijos[n]
+            if raiz.nodoLLeno():
+                raiz = self.__split(raiz,n)
+        return raiz
+
+    def __split(self, raiz, n):
+        if raiz.esHoja():
+            return self.__splitHoja(raiz, n)
+        return raiz
+
+    def __splitHoja(self, raiz, n):
+        #Se crea nuevo nodo y se separan las claves
+        m = int(self.orden/2)
+        nodo = Nodo(self.orden)
+        nodo.siguiente = raiz.siguiente
+        raiz.siguiente = nodo
+        nodo.claves = raiz.claves[m:]
+        raiz.claves = raiz.claves[0:m]
+        
+        if raiz.padre == None:
+            #Si el nodo padre es nulo se crea nuevo padre
+            padre = Nodo(self.orden)
+
+        else:
+            pass
+        return raiz
+
     def buscar(self, clave):
         return self.raiz.buscar(clave)
-  
+
