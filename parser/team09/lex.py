ebd410b53abd6ff0e1dcf7a37c889f7979071d3d
Lester Mazariegos <lestermaz.96@gmail.com>
2020-12-11 05:20:57 -0600

    Continuación de gramática
    Se agregaron las producciones delete, insert, update y otras requeridas para estas

--- parser/team09/lex.py
+++ parser/team09/lex.py
@@ -53,18 +53,45 @@ reservadas = {
     'without'   : 'WITHOUT',
     'zone'      : 'ZONE',
     'column '   : 'COLUMN',
-    'add'       : 'ADD'
+    'add'       : 'ADD',
+    'delete'    : 'DELETE',
+    'from'      : 'FROM',
+    'where'     : 'WHERE',
+    'insert'    : 'INSERT',
+    'into'      : 'INTO',
+    'values'    : 'VALUES',
+    'update'    : 'UPDATE',
+    'set'       : 'SET',
+    'and'       : 'AND',
+    'sum'       : 'SUM',
+    'avg'       : 'AVG',
+    'max'       : 'MAX',
+    'pi'        : 'PI',
+    'power'     : 'POWER',
+    'sqrt'      : 'SQRT'
 }
 
 # Lista de tokens
 tokens = [
-    'COMA', 
-    'PTCOMA', 
-    'PARDER', 
-    'PARIZQ', 
-    'CADENA',
+    'COMA',
+    'PARIZQ',
+    'PARDER',
+    'PTCOMA',
+    'MAYIG',
+    'MENIG',
+    'IGUAQ',
+    'DIFEQ',
+    'MAYOR',
+    'MENOR',
     'IGUAL',
+    'MULTI',
+    'MENOS',
+    'SUMAS',
+    'DIVIS',
+    'POTEN',
+    'CADENA',
     'ID',
+    'DECIMA',
     'ENTERO'
 ] + list(reservadas.values())
 
@@ -72,8 +99,19 @@ tokens = [
 t_COMA = r','
 t_PARIZQ = r'\('
 t_PARDER = r'\)'
-t_IGUAL = r'='
 t_PTCOMA = r';'
+t_MAYIG = r'>='
+t_MENIG = r'<='
+t_IGUAQ = r'=='
+t_DIFEQ = r'<>'
+t_MAYOR = r'>'
+t_MENOR = r'<'
+t_IGUAL = r'='
+t_MULTI = r'\*'
+t_MENOS = r'-'
+t_SUMAS = r'\+'
+t_DIVIS = r'/'
+t_POTEN = r'\^'
 
 t_ignore = " \t"
 
@@ -87,6 +125,15 @@ def t_CADENA(t):
     t.value = t.value[1:-1] # Remueve las comillas
     return t 
 
+def t_DECIMA(t):
+    r'\d+[.]\d+'
+    try:
+        t.value = float(t.value)
+    except ValueError:
+        print("Float value too large %d", t.value)
+        t.value = 0
+    return t
+
 def t_ENTERO(t):
     r'\d+'
     try:
@@ -95,7 +142,6 @@ def t_ENTERO(t):
         print("Integer value too large %d", t.value)
         t.value = 0
     return t
-
 
 def t_nuevalinea(t):
     r'\n+'
@@ -109,7 +155,20 @@ def t_error(t):
 # Construyendo el analizador léxico
 import ply.lex as lex
 import re
-lexer = lex.lex(reflags=re.IGNORECASE) 
+lexer = lex.lex(reflags=re.IGNORECASE)
+
+
+# Asociación de operadores y precedencia
+precedence = (
+    ('left','SUMAS','MENOS'),
+    ('left','MULTI','DIVIS'),
+    ('left','POTEN'),
+    ('right','UMENOS', 'USUMAS'),
+    ('left','MAYIG','MENIG','IGUAQ','DIFEQ','MAYOR','MENOR'),
+    ('right','NOT'),
+    ('left','AND'),
+    ('left','OR'),
+    ) 
 
 # Definir gramática
 
@@ -122,6 +181,9 @@ def p_entrada(t):
                 | entrada create_table
                 | entrada drop_table
                 | entrada alter_table
+                | entrada s_delete
+                | entrada s_insert
+                | entrada s_update
                 | create_type
                 | create_db
                 | show_db
@@ -129,7 +191,10 @@ def p_entrada(t):
                 | drop_db 
                 | create_table
                 | drop_table
-                | alter_table'''
+                | alter_table
+                | s_delete
+                | s_insert
+                | s_update'''
     print("Cadena correcta")
 
 def p_create_type(t):
@@ -285,6 +350,61 @@ def p_acc(t):
     '''acc  : const
             | const_keys'''
 
+def p_delete(t):
+    '''s_delete : DELETE FROM ID PTCOMA
+                | DELETE FROM ID WHERE ID IGUAL expresion PTCOMA '''
+
+def p_insert(t):
+    '''s_insert : INSERT INTO ID PARIZQ lista_id PARDER VALUES lista_values PTCOMA '''
+                #| INSERT INTO ID PARIZQ lista_id PARDER s_select''' 
+
+def p_lista_values(t):
+    '''lista_values : lista_values COMA PARIZQ lista_valores PARDER
+                     | PARIZQ lista_valores PARDER'''
+
+def p_lista_valores(t):
+    '''lista_valores : lista_valores COMA valores
+                     | valores'''
+
+def p_valores(t):
+    '''valores : CADENA
+               | ENTERO
+               | DECIMA'''
+
+def p_s_update(t):
+    '''s_update : UPDATE ID SET lista_asig PTCOMA
+                | UPDATE ID SET lista_asig WHERE ID IGUAL expresion PTCOMA'''
+
+def p_lista_asig(t):
+    '''lista_asig : lista_asig COMA ID IGUAL valores
+                  | ID IGUAL valores'''
+
+def p_expresion(t):
+    '''expresion : NOT expresion
+                 | expresion OR expresion
+                 | expresion AND expresion
+                 | expresion MAYOR expresion
+                 | expresion MENOR expresion
+                 | expresion MAYIG expresion
+                 | expresion MENIG expresion
+                 | expresion IGUAQ expresion
+                 | expresion DIFEQ expresion
+                 | MENOS expresion %prec UMENOS
+                 | SUMAS expresion %prec USUMAS
+                 | expresion POTEN expresion
+                 | expresion MULTI expresion
+                 | expresion DIVIS expresion
+                 | expresion SUMAS expresion
+                 | expresion MENOS expresion
+                 | PARIZQ expresion PARDER
+                 | SUM PARIZQ expresion PARDER
+                 | AVG PARIZQ expresion PARDER
+                 | MAX PARIZQ expresion PARDER
+                 | PI
+                 | POWER PARIZQ expresion PARDER
+                 | SQRT PARIZQ expresion PARDER
+                 | valores'''
+
 def p_error(t):
     print(t)
     print("Error sintáctico en '%s'" % t.value)
@@ -293,4 +413,4 @@ def p_error(t):
 import ply.yacc as yacc
 parser = yacc.yacc()
 
-parser.parse("CREATE TYPE inventory_item AS ( 'name text', supplier_id integer, price numeric ) ; CREATE DATABASE name; SHOW DATABASES; SHOW DATAbASES like gg; ALTER DATABASE name RENAME TO new_name; ALTER DATABASE name OWNER TO SESSION_USER; CREATE TABLE my_first_table ( column1 integer PRIMARY KEY, column2 numeric REFERENCES table2,column3 text,column4 varchar(10));CREATE TABLE my_first_table (column1 integer PRIMARY KEY, column2 numeric REFERENCES table3, column3 text, column4 varchar(10) NOT NULL, column5 integer NULL, column4 varchar(10) NOT NULL CONSTRAINT gg UNIQUE);")
+parser.parse("CREATE TYPE inventory_item AS ( 'name text', supplier_id integer, price numeric ) ; CREATE DATABASE name; SHOW DATABASES; SHOW DATAbASES like gg; ALTER DATABASE name RENAME TO new_name; ALTER DATABASE name OWNER TO SESSION_USER; CREATE TABLE my_first_table ( column1 integer PRIMARY KEY, column2 numeric REFERENCES table2,column3 text,column4 varchar(10));CREATE TABLE my_first_table (column1 integer PRIMARY KEY, column2 numeric REFERENCES table3, column3 text, column4 varchar(10) NOT NULL, column5 integer NULL, column4 varchar(10) NOT NULL CONSTRAINT gg UNIQUE); INSERT INTO tabla1 (campo1, campo2) VALUES('valor1', 1), ('valor2', 2); UPDATE tabla1 SET campo2 = 3 WHERE campo2 = 2; DELETE FROM tabla1 WHERE campo2 = 3;")
