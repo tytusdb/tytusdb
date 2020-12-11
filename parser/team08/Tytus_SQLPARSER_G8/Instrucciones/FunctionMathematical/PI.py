e87e6544f7f06616e274637dd5c6fd8a9821620f
michikatrins <michikatrins@gmail.com>
2020-12-10 19:34:38 -0600

    agregacion de grafica y agrupaciones de clases

---
+++ parser/team08/Tytus_SQLPARSER_G8/Instrucciones/FunctionMathematical/PI.py
@@ -0,0 +1,20 @@
+from enum import Enum
+import math
+
+
+from Instrucciones.TablaSimbolos.Instruccion import Instruccion
+class PI(Instruccion):
+    def __init__(self, valor, tipo, linea, columna):
+        Instruccion.__init__(self,tipo,linea,columna)
+        self.valor = valor
+
+    def ejecutar(self, tabla, arbol):
+        super().ejecutar(tabla,arbol)
+        print("PI")
+        print(self.valor*math.pi)
+        resultado = self.valor*math.pi
+        return resultado
+
+
+Instruccion = PI(1,None, 1,2)
+Instruccion.ejecutar(None,None)
