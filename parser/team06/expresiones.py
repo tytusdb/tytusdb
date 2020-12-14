43e66c5b38d6c12845b88fa015b978a822fbde5b
Jossie Bismarck Castrillo Fajardo <bkcastrillo@gmail.com>
2020-12-14 11:33:23 -0600

    Actualizacion Semantica
    Se ejecutan 2 tipos de sentencia select, todas las funciones basicas, se genera el reporte de la tabla de simbolos con los datos almacenados.
    Se agregaron los objetos para la ejecucion en las clases queries y expresiones.

--- parser/team06/expresiones.py
+++ parser/team06/expresiones.py
@@ -35,3 +35,432 @@ class OPERACION_FUNCION_DEFINIDA(Enum):
     #FALTA EL MONTON
 
 
+class ExpresionNumerica:
+    '''
+        Esta clase representa una expresión numérica
+    '''
+class ExpresionNegativo(ExpresionNumerica) :
+    '''
+        Esta clase representa la Expresión Aritmética Negativa
+        Esta clase recibe la expresion
+    '''
+    def __init__(self, id) :
+        self.id = id
+
+class ExpresionNumero(ExpresionNumerica) :
+    '''
+        Esta clase representa una expresión numérica entera o decimal.
+    '''
+
+    def __init__(self, id = 0) :
+        self.id = id
+
+class ExpresionIdentificador(ExpresionNumerica) :
+    '''
+        Esta clase representa un identificador.
+    '''
+
+    def __init__(self, id = "") :
+        self.id = id
+
+
+
+class ExpresionFuncionBasica(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, id) :
+        self.id = id
+
+class ExpresionCadena :
+    '''
+        Esta clase representa una Expresión de tipo cadena.
+    '''
+
+
+class ExpresionCadenas(ExpresionCadena) :
+    '''
+        Esta clase representa una cadena entre comillas simples.
+        Recibe como parámetro el valor del token procesado por el analizador léxico
+    '''
+
+    def __init__(self, id) :
+        self.id = id
+
+# ---------------------------------------------------------------------------------------------------------------------
+#                                EXPRESIONES DE LAS OPERACIONES BASICAS PARA EL SELECT
+# ---------------------------------------------------------------------------------------------------------------------
+
+class ExpresionABS(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionCBRT(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionCEIL(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionCEILING(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionDEGREES(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionDIV(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp1,exp2) :
+        self.exp1 = exp1
+        self.exp2 = exp2
+
+class ExpresionEXP(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionFACTORIAL(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionFLOOR(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionGCD(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp1,exp2) :
+        self.exp1 = exp1
+        self.exp2 = exp2
+
+class ExpresionLN(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionLOG(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionMOD(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp1,exp2) :
+        self.exp1 = exp1
+        self.exp2 = exp2
+
+class ExpresionPI(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+
+class ExpresionPOWER(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp1,exp2) :
+        self.exp1 = exp1
+        self.exp2 = exp2
+
+
+class ExpresionRADIANS(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionROUND(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionSIGN(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionSQRT(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionTRUNC(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionRANDOM(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionWIDTHBUCKET(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp1,exp2,exp3,exp4) :
+        self.exp1 = exp1
+        self.exp2 = exp2
+        self.exp3 = exp3
+        self.exp4 = exp4
+
+# ----------------------------------------------------------------
+#           EMPIEZAN LAS TRIGONOMETRICAS
+
+class ExpresionACOS(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionACOSD(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionASIN(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionASIND(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionATAN(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionATAND(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionATAN2(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp1,exp2) :
+        self.exp1 = exp1
+        self.exp2 = exp2
+
+
+class ExpresionATAN2D(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp1,exp2) :
+        self.exp1 = exp1
+        self.exp2 = exp2
+
+
+
+class ExpresionCOS(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionCOSD(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionCOT(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionCOTD(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionSIN(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionSIND(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionTAN(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionTAND(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionSINH(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionCOSH(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+
+class ExpresionTANH(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionASINH(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionACOSH(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionATANH(ExpresionNumerica):
+    '''
+        Esta clase representa la expresión para castear datos.
+        Esta clase recibe un tipo a convertir y el dato
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+# ----------------------------------------------------------------
+#               BINARY STRING FUNCTIONS
--- parser/team06/queries.py
+++ parser/team06/queries.py
@@ -9,3 +9,35 @@ class ShowDatabases(query) :
 
     def __init__(self, variable) :
         self.variable = variable
+
+class Select(query) :
+    '''
+        Esta clase representa una accion que elimina la variable
+        Recibe como parámetro la variable como tal
+    '''
+
+    def __init__(self, tipo, operacion) :
+        self.tipo = tipo
+        self.operacion = operacion
+
+class Select2(query) :
+    '''
+        Esta clase representa una accion que elimina la variable
+        Recibe como parámetro la variable como tal
+    '''
+
+    def __init__(self, tipo, operacion1, operacion2) :
+        self.tipo = tipo
+        self.operacion1 = operacion2
+        self.operacion2 = operacion2
+
+
+class Asignacion(query) :
+    '''
+        Esta clase representa la instruccion de una asignacion AS
+        recibe como parametro el campo y su nuevo nombre a manejar
+    '''
+    def __init__(self, campo, alias) :
+        self.campo = campo
+        self.alias = alias
+
