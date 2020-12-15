fd0832bb2b3599f5db3c69f3e81c0f4caacbc83b
Luis Castellanos <luis.danniel@hotmail.com>
2020-12-15 01:54:13 -0600

    [SG16] Table module - Manejo de archivos
    Algunas funciones creadas de tablas, corregidas las de base de datos.

--- storage/team16/BusinessLayer/database_module.py
+++ storage/team16/BusinessLayer/database_module.py
@@ -1,10 +1,10 @@
 from DataAccessLayer.handler import Handler
-
+from BusinessLayer.table_module import TableModule
 
 class Database:
-    def __init__(self, name):
-        self.name = str(name)
-        self.tables = []
+    def __init__(self, name, tablesName):
+        self.name = name
+        self.tablesName = tablesName
 
     def __repr__(self) -> str:
         return str(self.name)
@@ -13,51 +13,59 @@ class Database:
 class DatabaseModule:
     def __init__(self):
         self.handler = Handler()
-        self.databases = self.handler.rootInstance()
+        self.databases = self.handler.leerArchivoDB()
 
-    def createDatabase(self, database):
+    
+    def createDatabase(self, database: str) -> int:
         try:
-            for element in self.databases:
-                if database == element.name:
-                    return 2
-            self.databases.append(Database(database))
-            self.handler.rootUpdate(self.databases)
+            for i in self.databases:
+                if database == i.name:
+                    return  2
+            self.databases.append(Database(database,[]))
+            self.handler.actualizarArchivoDB(self.databases)
             return 0
         except:
             return 1
 
-    def showDatabases(self):
-        tmp = []
-        for database in self.databases:
-            tmp.append(database.name)
-        return tmp
+    def showDatabases(self) -> list:
+        temporal = []
+        for i in self.databases:
+            temporal.append(i.name)
+        print(temporal)
+        return temporal
 
-    def alterDatabase(self, old, new):
+    def alterDatabase(self, databaseOld: str, databaseNew: str) -> int:
         try:
-            index = -1
-            for database in self.databases:
-                if str(new) == database.name:
+            for i in self.databases:
+                if str(databaseNew) == str(i.name):
                     return 3
-
-            for database in self.databases:
-                if str(old) == database.name:
-                    index = self.databases.index(database)
-                    break
-            if index != -1:
-                self.databases[index].name = new
-                self.handler.rootUpdate(self.databases)
-                return 0
+            for i in self.databases:
+                if str(databaseOld) == str(i.name):
+                    # print(i.tablesName)
+                    tables_temp = self.handler.findCoincidences(databaseOld,i.tablesName)
+                    i.name = databaseNew
+                    for j in tables_temp:
+                        avl_temp = self.handler.leerArchivoTB(databaseOld,j)
+                        avl_temp.database = databaseNew
+                        self.handler.renombrarArchivo(str(j)+'-'+str(databaseOld)+'.tbl',str(j)+'-'+str(databaseNew)+'.tbl')
+                    self.handler.actualizarArchivoDB(self.databases)
+                    return 0
             return 2
         except:
             return 1
 
-    def dropDatabase(self, database):
+    def dropDatabase(self, database: str) -> int:
         try:
+            aux = 0
             for i in range(len(self.databases)):
-                if str(database) == self.databases[i].name:
+                if str(database) == str(self.databases[i].name):
+                    tables_temp = self.handler.findCoincidences(database,self.databases[i].tablesName)
                     self.databases.pop(i)
-                    self.handler.rootUpdate(self.databases)
+                    for j in tables_temp:
+                        self.handler.borrarArchivo(str(j)+'-'+str(database)+'.tbl')
+                    self.handler.actualizarArchivoDB(self.databases)
                     return 0
+                aux +=1
             return 2
         except:
             return 1
