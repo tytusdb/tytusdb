ada86cb49413b26e4608e4ce076c7769bf2cfb1a
Luis Castellanos <luis.danniel@hotmail.com>
2020-12-15 01:54:13 -0600

    Revert "[SG16] Table module - Manejo de archivos"
    This reverts commit fd0832bb2b3599f5db3c69f3e81c0f4caacbc83b.

--- storage/team16/DataAccessLayer/handler.py
+++ storage/team16/DataAccessLayer/handler.py
@@ -1,68 +1,34 @@
 import pickle
 import os
 
-class Handler:
 
-    #Databases
+class Handler:
     @staticmethod
-    def actualizarArchivoDB(databases):
-        f = open('data/root.dat','wb')
-        pickle.dump(databases, f)
-        f.close()
-    
-    @staticmethod
-    def leerArchivoDB() -> list:
+    def rootInstance():
         if not os.path.exists('data'):
             os.makedirs('data')
         if not os.path.exists('data/root.dat'):
-            f = open('data/root.dat','wb')
+            data = []
+            with open('data/root.dat', 'wb') as file:
+                pickle.dump(data, file)
+            return []
+        with open('data/root.dat', 'rb') as file:
+            return pickle.load(file)
+
+    @staticmethod
+    def rootUpdate(databases):
+        file = open('data/root.dat', 'wb')
+        pickle.dump(databases, file)
+        file.close()
+        return databases
+
+    @staticmethod
+    def leerArchivoDB() -> list:
+        try:
+            if os.path.getsize('databases') > 0:
+                with open('databases', 'rb') as f:
+                    return pickle.load(f)
+            return []
+        except:
+            f = open("databases", 'wb')
             f.close()
-        if os.path.getsize('data/root.dat') > 0:
-            with open('data/root.dat', 'rb') as f:
-                return pickle.load(f)
-        return []
-
-
-    #Tables
-    @staticmethod
-    def actualizarArchivoTB(tabletrees, database: str, tableName: str):
-        f = open('data/'+str(tableName)+'-'+str(database)+'.tbl','wb')
-        pickle.dump(tabletrees, f)
-        f.close()
-
-    @staticmethod
-    def siExiste(database: str, tableName: str):
-        return os.path.isfile('data/'+str(tableName)+'-'+str(database)+'.tbl')
-
-    @staticmethod
-    def leerArchivoTB(database: str, tableName: str):
-        if os.path.getsize('data/'+str(tableName)+'-'+str(database)+'.tbl') > 0:
-            with open('data/'+str(tableName)+'-'+str(database)+'.tbl', 'rb') as f:
-                return pickle.load(f)
-        else:
-            return None
-    
-    @staticmethod
-    def borrarArchivo(filename):
-        try:
-            os.remove('data/'+filename)
-        except:
-            print("No se encontró el archivo")
-
-    @staticmethod
-    def renombrarArchivo(oldName, newName):
-        try:
-            os.rename('data/'+oldName, 'data/'+newName)
-        except:
-            print("No se pudo renombrar")
-
-    @staticmethod
-    def findCoincidences(database, tablesName):
-        tmp = []
-        for i in tablesName:
-            try:
-                if os.path.isfile('data/'+str(i)+'-'+str(database)+'.tbl'):
-                    tmp.append(str(i))
-            except:
-                continue
-        return tmp
