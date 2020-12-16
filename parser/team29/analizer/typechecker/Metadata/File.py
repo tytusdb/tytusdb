93f7b717e4c4facd15f74ab50284b16cbeb676aa
Francisco Suarez <39706929+Yosoyfr@users.noreply.github.com>
2020-12-16 02:09:21 -0600

    [FEAT]: Implementacion del CREATE DATABASE
    Ejecucuion del CREATE DATABASE con las reglas semanticas y verificacion de errores, integracion con el STORAGE.
    
    Co-Authored-By: Estela-Beatriz <57296308+Estela-Beatriz@users.noreply.github.com>

--- parser/team29/analizer/typechecker/Metadata/File.py
+++ parser/team29/analizer/typechecker/Metadata/File.py
@@ -1,21 +1,13 @@
 import json
 
-def updateFile(Databases):
-    dbTemp = importFile()
-    for db in dbTemp:
-        Databases.append(db)
-
-    with open('Databases.json','w') as file:
-        json.dump(Databases,file)
-        print("Update")
 
 def exportFile(Databases):
-    with open('Databases.json','w') as file:
-        json.dump(Databases,file)
+    with open("Databases.json", "w") as file:
+        json.dump(Databases, file)
         print("Export")
 
+
 def importFile():
-    with open('Databases.json','r') as file:
+    with open("Databases.json", "r") as file:
         databases = json.load(file)
         return databases
-        
