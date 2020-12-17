6b6dfc01554079c45d51ad5bc2afa1d246545fa6
Gleimy Polanco <gleypolanco@gmail.com>
2020-12-16 20:50:53 -0600

    Form para Base de datos
    Formulario para base de datos

--- storage/team02/Interfaz/Interfaz.py
+++ storage/team02/Interfaz/Interfaz.py
@@ -7,4 +7,27 @@ VentanaPrincipal.config(background = "#f9e0ae")
 raiz.config(background = "#f9e0ae")
 VentanaPrincipal.pack()
 
+#......................Creaci[on de Campos para Bases de DATOS
+
+nomBaseDatos2 = Entry(VentanaPrincipal)
+nomBaseDatos2.place(x = 810 , y = 170)
+nomBaseDatos2.config(relief = "sunken", borderwidth = 4)
+nomBaseDatos2Label = Label(VentanaPrincipal, text="Nombre BD:")
+nomBaseDatos2Label.place(x = 730 , y = 170)
+nomBaseDatos2Label.config(background = "#f9e0ae" , foreground = "#682c0e", font = ("Helvetica", 9, "bold"))
+
+RenomBaseDatos2 = Entry(VentanaPrincipal)
+RenomBaseDatos2.place(x = 810 , y = 240)
+RenomBaseDatos2.config(relief = "sunken", borderwidth = 4)
+RenomBaseDatos2Label = Label(VentanaPrincipal, text="Renombrar:")
+RenomBaseDatos2Label.place(x = 730 , y = 240)
+RenomBaseDatos2Label.config(background = "#f9e0ae" , foreground = "#682c0e", font = ("Helvetica", 9, "bold"))
+
+BuscarBaseDatos2 = Entry(VentanaPrincipal)
+BuscarBaseDatos2.place(x = 810 , y = 205)
+BuscarBaseDatos2.config(relief = "sunken", borderwidth = 4)
+BuscarBaseDatos2Label = Label(VentanaPrincipal, text="Buscar:")
+BuscarBaseDatos2Label.place(x = 730 , y = 205)
+BuscarBaseDatos2Label.config(background = "#f9e0ae" , foreground = "#682c0e", font = ("Helvetica", 9, "bold"))
+
 raiz.mainloop()
