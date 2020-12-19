9402632e2a5e6e1ffad006d315f49fc9af329b39
Josue Abelarde <datosmoviles52@gmail.com>
2020-12-19 10:31:51 -0600

    instalando dependencias e iniciando con el compomente donde visualizaremos la data de respuesta

--- client/team05/src/app/app.module.ts
+++ client/team05/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DataComponent } from './components/data/data.component';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
