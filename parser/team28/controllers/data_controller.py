46d10f7940161b8fc62150ab88c066dc98edf2c8
Didier Domínguez <dadu0699@hotmail.com>
2020-12-19 12:15:02 -0600

    [G28] Resultados en consola

--- parser/team28/controllers/data_controller.py
+++ parser/team28/controllers/data_controller.py
@@ -2,6 +2,7 @@ from controllers.error_controller import ErrorController
 from controllers.type_checker import TypeChecker
 from controllers.symbol_table import SymbolTable
 from controllers import data_mode
+from views.data_window import DataWindow
 
 
 class DataController(object):
@@ -32,15 +33,15 @@ class DataController(object):
         database = SymbolTable().useDatabase
         if not database:
             desc = f": Database not selected"
-            ErrorController().addExecutionError(4, 'Execution', desc,
-                                                noLine, noColumn)
+            ErrorController().add(4, 'Execution', desc,
+                                  noLine, noColumn)
             return None
 
         tables = data_mode.mode(database.mode).showTables(database.name)
 
         if tables == None:
             desc = f": Database {database.name} does not exist"
-            ErrorController().addExecutionError(35, 'Execution', desc, noLine, noColumn)
+            ErrorController().add(35, 'Execution', desc, noLine, noColumn)
             return None
 
         return tables
@@ -57,17 +58,17 @@ class DataController(object):
         database = SymbolTable().useDatabase
         if not database:
             desc = f": Database not selected"
-            ErrorController().addExecutionError(4, 'Execution', desc,
-                                                noLine, noColumn)
+            ErrorController().add(4, 'Execution', desc,
+                                  noLine, noColumn)
             return None
 
         if not TypeChecker().searchTable(database, name):
             desc = f": Table {name} does not exist"
-            ErrorController().addExecutionError(27, 'Execution', desc, noLine, noColumn)
+            ErrorController().add(27, 'Execution', desc, noLine, noColumn)
 
         data = data_mode.mode(database.mode).extractTable(database.name, name)
         if data == None:
-            ErrorController().addExecutionError(34, 'Execution', '', noLine, noColumn)
+            ErrorController().add(34, 'Execution', '', noLine, noColumn)
             return None
 
         return data
@@ -87,13 +88,13 @@ class DataController(object):
         database = SymbolTable().useDatabase
         if not database:
             desc = f": Database not selected"
-            ErrorController().addExecutionError(4, 'Execution', desc,
-                                                noLine, noColumn)
+            ErrorController().add(4, 'Execution', desc,
+                                  noLine, noColumn)
             return None
 
         if not TypeChecker().searchTable(database, name):
             desc = f": Table {name} does not exist"
-            ErrorController().addExecutionError(27, 'Execution', desc, noLine, noColumn)
+            ErrorController().add(27, 'Execution', desc, noLine, noColumn)
 
     def alterAddPK(self, name: str, columns: list, noLine, noColumn):
         """
@@ -108,40 +109,42 @@ class DataController(object):
         database = SymbolTable().useDatabase
         if not database:
             desc = f": Database not selected"
-            ErrorController().addExecutionError(4, 'Execution', desc,
-                                                noLine, noColumn)
-            return None
+            ErrorController().add(4, 'Execution', desc,
+                                  noLine, noColumn)
+            return
 
         if not TypeChecker().searchTable(database, name):
             desc = f": Table {name} does not exist"
-            ErrorController().addExecutionError(27, 'Execution', desc, noLine, noColumn)
+            ErrorController().add(27, 'Execution', desc, noLine, noColumn)
+            return
 
         dbStatement = data_mode.mode(database.mode).alterAddPK(database.name,
                                                                name, columns)
 
         if dbStatement == 0:
-            print('Query returned successfully: Alter Table add PK')
+            DataWindow().consoleText('Query returned successfully: Alter Table add PK')
 
         elif dbStatement == 1:
-            ErrorController().addExecutionError(34, 'Execution', desc, noLine, noColumn)
+            ErrorController().add(34, 'Execution', '', noLine, noColumn)
 
         elif dbStatement == 2:
             desc = f": Database {database.name} does not exist"
-            ErrorController().addExecutionError(35, 'Execution', desc, noLine, noColumn)
+            ErrorController().add(35, 'Execution', desc, noLine, noColumn)
 
         elif dbStatement == 3:
             desc = f": Table {name} does not exist"
-            ErrorController().addExecutionError(27, 'Execution', desc, noLine, noColumn)
+            ErrorController().add(27, 'Execution', desc, noLine, noColumn)
 
         elif dbStatement == 4:
             desc = f": Multiple primary keys for table {database.name} are not allowed"
-            ErrorController().addExecutionError(36, 'Execution', desc, noLine, noColumn)
+            ErrorController().add(36, 'Execution', desc, noLine, noColumn)
 
         elif dbStatement == 5:
             desc = f": Column of relation {name} does not exist"
-            ErrorController().addExecutionError(26, 'Execution', desc, noLine, noColumn)
+            ErrorController().add(26, 'Execution', desc, noLine, noColumn)
 
     def alterDropPK(self, database: str, table: str) -> int:
+        # TODO TERMINAR
         pass
 
     def insert(self, name: str, data: list, noLine, noColumn):
@@ -157,36 +160,31 @@ class DataController(object):
         database = SymbolTable().useDatabase
         if not database:
             desc = f": Database not selected"
-            ErrorController().addExecutionError(4, 'Execution', desc,
-                                                noLine, noColumn)
+            ErrorController().add(4, 'Execution', desc,
+                                  noLine, noColumn)
             return None
 
         dbStatement = data_mode.mode(database.mode).insert(database.name,
                                                            name, data)
 
         if dbStatement == 0:
-            print('Query returned successfully: INSERT INTO')
+            DataWindow().consoleText('Query returned successfully: INSERT INTO')
 
         elif dbStatement == 1:
-            print('msg', 1)
-            #ErrorController().addExecutionError(34, 'Execution', desc, noLine, noColumn)
+            ErrorController().add(34, 'Execution', '', noLine, noColumn)
 
         elif dbStatement == 2:
-            print('msg', 2)
-            #desc = f": Database {database.name} does not exist"
-            #ErrorController().addExecutionError(35, 'Execution', desc, noLine, noColumn)
+            desc = f": Database {database.name} does not exist"
+            ErrorController().add(35, 'Execution', desc, noLine, noColumn)
 
         elif dbStatement == 3:
-            print('msg', 3)
-            #desc = f": Table {name} does not exist"
-           # ErrorController().addExecutionError(27, 'Execution', desc, noLine, noColumn)
+            desc = f": Table {name} does not exist"
+            ErrorController().add(27, 'Execution', desc, noLine, noColumn)
 
         elif dbStatement == 4:
-            print('msg', 4)
-            #desc = f": Multiple primary keys for table {database.name} are not allowed"
-           # ErrorController().addExecutionError(36, 'Execution', desc, noLine, noColumn)
+            desc = f": Duplicate key value violates unique"
+            ErrorController().add(24, 'Execution', desc, noLine, noColumn)
 
         elif dbStatement == 5:
-            print('msg', 5)
-            #desc = f": Column of relation {name} does not exist"
-            #ErrorController().addExecutionError(26, 'Execution', desc, noLine, noColumn)
+            desc = f": Column of relation {name} does not exist"
+            ErrorController().add(26, 'Execution', desc, noLine, noColumn)
