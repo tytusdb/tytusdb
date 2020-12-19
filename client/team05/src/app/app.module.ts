41e5ed3067f5be0823f3b256bbe258ee608df338
Josue Abelarde <datosmoviles52@gmail.com>
2020-12-19 14:02:51 -0600

    Agregando la tabla para mostrar los datos

--- client/team05/src/app/app.module.ts
+++ client/team05/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DataComponent } from './components/data/data.component';
+import { DataComponent } from './components/tabla/data.component';
 import { MaterialModule } from './material/material.module';
 import { MatButtonModule } from '@angular/material/button'
 import { EditorComponent } from './editor/editor.component';
