c6b63ec02e3869329eb9e4f5a0eae0193fe8aaf9
bmoisesg <bmoisesg@gmail.com>
2020-12-20 04:41:49 -0600

    list database

--- client/team05/src/app/components/tree/tree.component.ts
+++ client/team05/src/app/components/tree/tree.component.ts
@@ -1,3 +1,4 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TreeComponent implements OnInit {
 
-  constructor() { }
+  cadena_db: String = ""
 
-  ngOnInit(): void {
+  constructor() { 
   }
+  
+  ngOnInit(): void {
+    this.funcion1()
+
+  }
+
+   funcion1() {
+    var toggler = document.getElementsByClassName("caret");
+    var i;
+
+    for (i = 0; i < toggler.length; i++) {
+      toggler[i].addEventListener("click", function () {
+        this.parentElement.querySelector(".animacion").classList.toggle("activada");
+        this.classList.toggle("caret-down");
+      });
+    }
+  }
+
+
+  
+
+   refrescar() {
+    alert("def showDatabases()")
+
+    let item_db = document.getElementById("db")
+    item_db.innerHTML=""
+
+    this.cadena_db = "[basedatos1,basedatos2, basedatos3]";
+
+    // analizando respuesta
+    let array = this.analizar(this.cadena_db)
+    console.log("data bases : ", array)
+
+    //  actualizando el numero de base de datos 
+    let numero_db = document.getElementById("num_db")
+    numero_db.innerText = String(array.length)
+
+    for (let i = 0; i < array.length; i++) {
+      item_db.innerHTML += '<li><span class="caret">' + array[i] + '</span><ul class="animacion"><li><span class="caret">Tabla []</span><ul class="animacion" id="' + array[i] + '"></ul></li></ul></li>'
+    }
+    this.funcion1()
+
+  }
+
+  analizar(data) {
+    var array = []
+    data = data.replace('[', "")
+    data = data.replace(']', "")
+    var list_data = data.split(',')
+    for (let i = 0; i < list_data.length; i++) {
+      array.push(list_data[i])
+    }
+    return array
+  }
+
 
 }
