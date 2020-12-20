21a157e0465136c98d8d0fff5bbeb1ec228f5be3
201700670 <vgdep1997@gmail.com>
2020-12-19 19:44:33 -0600

    create button of play and add the exit for the application with the message

--- client/team08/index .py
+++ client/team08/index .py
@@ -1,85 +1,94 @@
-from tkinter import ttk 
 import tkinter as tk
-from tkinter import *
-from tkinter import scrolledtext
-import tkinter.scrolledtext as tkscrolled
+from tkinter import ttk
+from tkinter import scrolledtext as st
 from tkinter import messagebox
 from tkinter import Button
-# pip install pillow
+from tkinter import LabelFrame
+from tkinter import Label
+from tkinter import Menu
 from PIL import ImageTk, Image
-class base:
-    #esto es un constructor
-    def __init__(self, window):
-        self.wind = window #guarda la ventana que tiene como parametro  
-        self.wind.title('TytusDB')
-        load = Image.open("icondb.png")
-        self.wind.iconphoto(False, ImageTk.PhotoImage(load))
-        #creando el contenedor de barra de herramientas superior
-        #columnspam para un espaciado dentro de las columnass
-        #padding espacio entre cada elemento
 
-        #self.toolbar = Frame(root,bg="gray",height=40)
-        #self.toolbar.pack(side=TOP, fill=X)
-        Bases = LabelFrame(self.wind, text= 'BASES DE DATOS EXISTENTES', background="white")
-        Bases.grid(row=0, column=0, columnspan=3, pady=95)
+class Aplicacion:
+    def __init__(self):
+        self.ventana1=tk.Tk()
+        self.ventana1.title('TytusDB')
+        self.ventana1.geometry("850x450+100+100")
+        self.ventana1.configure(background='white')
+        #self.load = tk.Image.open("icondb.png")
+        #self.ventana1.iconphoto(False, ImageTk.PhotoImage(self.load))
+
+        #TEXTO DE ENTRADA
+        self.scrolledtext1=st.ScrolledText(self.ventana1, wrap = tk.WORD, width = 50, height = 7, font = ("Times New Roman", 15))
+        self.scrolledtext1.place(x=290, y=40)
+        self.framecopia()        
+
+        #TEXTO DE SALIDA
+        self.scrolledtext2=st.ScrolledText(self.ventana1, wrap = tk.WORD, width = 50, height = 7, font = ("Times New Roman", 15))
+        self.scrolledtext2.place(x=290, y=250) 
+
+        #MENU
+        self.menubar = Menu(self.ventana1,  background="gray")
+        self.ventana1.config(menu=self.menubar)
+        self.fileMenu = Menu(self.menubar)
+        self.fileMenu.add_command(label="Exit", command=self.Exit)
+        self.menubar.add_cascade(label="File", menu=self.fileMenu)
+        self.menubar.add_cascade(label="Object")
+        self.menubar.add_cascade(label="Tools")        
+        self.FileA = Menu(self.menubar)
+        self.FileA.add_command(label="Acerca de", command=self.MensajeAcercaDe)
+        self.menubar.add_cascade(label="Help", menu=self.FileA)
+
+        #BARRA DE HERRAMIENTAS DEL CENTRO
+        self.BarraHerramientas = LabelFrame(self.ventana1, text= '', background="white")
+        self.BarraHerramientas.grid(row=0, column=0, columnspan=3, pady=95)
+        self.BarraHerramientas.place(x=450, y=210)
+            #IMAGEN DEL CENTRO
+        self.load1 = Image.open("tytus.gif")
+        self.render1 = ImageTk.PhotoImage(self.load1)
+        self.img1 = Label(self.ventana1, image=self.render1)
+        self.img1.image = self.render1
+        self.img1.place(x=290, y=210)
+        tk.Label(self.ventana1, text = "Salida", font = ("Arial", 20), background = '#B5F5E0', foreground = "black").place(x=370, y=210)
+            #BOTON CORRER
+        self.boton1=ttk.Button(self.BarraHerramientas, text="RUN", command=self.copiar)
+        self.boton1.pack()
+
+        #IMAGEN
+        self.load = Image.open("icondb.png")
+        self.render = ImageTk.PhotoImage(self.load)
+        self.img = Label(self.ventana1, image=self.render)
+        self.img.image = self.render
+        self.img.place(x=0, y=0)
+
+        self.ventana1.iconphoto(False, ImageTk.PhotoImage(self.load))
+        self.ventana1.mainloop()
+
+#CONFIGURANDO PARA LA BASE DE DATOS.
+    def framecopia(self):
+        self.Bases = LabelFrame(self.ventana1, text= 'BASES DE DATOS EXISTENTES', background="white")
+        self.Bases.grid(row=0, column=0, columnspan=3, pady=95)
+        Label(self.Bases, text='BASES DE DATOS 1').grid(row=1, column=0)
+        Label(self.Bases, text='BASES DE DATOS 2').grid(row=2, column=0)
+        Label(self.Bases, text='BASES DE DATOS 3').grid(row=3, column=0)
+        Label(self.Bases, text='BASES DE DATOS 3.1').grid(row=4, column=1)
+        Label(self.Bases, text='BASES DE DATOS 3.2').grid(row=5, column=1)
+        Label(self.Bases, text='BASES DE DATOS 4').grid(row=6, column=0)   
+    
+    def copiar(self):
+        #iniciofila=self.dato1.get()
+        #iniciocolumna=self.dato2.get()
+        #finfila=self.dato3.get()
+        #fincolumna=self.dato4.get()        
+        datos=self.scrolledtext1.get(1.0, tk.END)
+        self.scrolledtext2.delete("1.0", tk.END)        
+        self.scrolledtext2.insert("1.0", datos)
+
+ 
         
-        text_area = scrolledtext.ScrolledText(self.wind, wrap = tk.WORD, width = 50, height = 7, font = ("Times New Roman", 15))                 
-        text_area.place(x=290, y=40)
-        text_area.focus() 
-        load1 = Image.open("tytus.gif")
-        render1 = ImageTk.PhotoImage(load1)
-        img1 = Label(self.wind, image=render1)
-        img1.image = render1
-        img1.place(x=290, y=215)
-        tk.Label(self.wind, text = "Salida", font = ("Arial", 20), background = '#B5F5E0', foreground = "black").place(x=370, y=215)
-        text_area1 = scrolledtext.ScrolledText(self.wind, wrap = tk.WORD, width = 50, height = 7, font = ("Times New Roman", 15))   
-        text_area1.place(x=290, y=250)         
-        text_area1.focus() 
-
-        Label(Bases, text='BASES DE DATOS 1').grid(row=1, column=0)
-        Label(Bases, text='BASES DE DATOS 2').grid(row=2, column=0)
-        Label(Bases, text='BASES DE DATOS 3').grid(row=3, column=0)
-        Label(Bases, text='BASES DE DATOS 3.1').grid(row=4, column=1)
-        Label(Bases, text='BASES DE DATOS 3.2').grid(row=5, column=1)
-        Label(Bases, text='BASES DE DATOS 4').grid(row=6, column=0)                
-
-        def MensajeAcercaDe():
-                messagebox.showinfo(message="ESTE PROGRAMA ES REALIZADO \n POR EL GRUPO 8:\n Cinthya Andrea Palomo Galvez 201700670 \n Karla Julissa Ajtún Velásquez 201700565 \n Version 1.0.0", title="TytusDB")
-        
-        correr = Button(self.wind, text="Sumar", command=MensajeAcercaDe)
-        correr.pack()
-
-
-        menubar =Menu(self.wind,  background="gray")
-        self.wind.config(menu=menubar)
-        fileMenu = Menu(menubar)
-        fileMenu.add_command(label="Exit", command= self.onExit)
-        menubar.add_cascade(label="File", menu=fileMenu)
-        menubar.add_cascade(label="Object")
-        menubar.add_cascade(label="Tools")        
-        FileA = Menu(menubar)
-        FileA.add_command(label="Acerca de", command=MensajeAcercaDe)
-        menubar.add_cascade(label="Help", menu=FileA)
-
-
-        #------IMAGE-------
-
-        
-        render = ImageTk.PhotoImage(load)
-        img = Label(self.wind, image=render)
-        img.image = render
-        img.place(x=0, y=0)
-        #------TEXTAREA SALIDA------
-       # Vertical (y) Scroll Bar
-        
-    def onExit(self):
-
-        self.quit()
-
-if __name__ == '__main__':
-        window = Tk()
-        window.geometry("850x450+100+100")
-
-        window.configure(background='white')
-        application = base(window)
-        window.mainloop()
+    def MensajeAcercaDe(self):          
+                  
+                messagebox.showinfo(message="ESTE PROGRAMA ES REALIZADO \n POR EL GRUPO 8:\n  \n Version 1.0.0", title="TytusDB")
+    def Exit(self):
+            messagebox.showinfo(message="Gracias por utilizar este programa! :v", title="TytusDB")
+            quit();
+aplicacion1=Aplicacion()
