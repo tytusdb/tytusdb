5f7b2a4a136508570a8a7cc839afcd818f087a13
Jose Moran <3005377270101@ingenieria.usac,edu.gt>
2020-12-20 00:37:46 -0600

    [BD1_11]Add ace editor for querys

--- server/team11/tytusweb/node_modules/@angular-devkit/build-angular/src/webpack/configs/server.d.ts
+++
@@ -1,7 +0,0 @@
-import { Configuration } from 'webpack';
-import { WebpackConfigOptions } from '../../utils/build-options';
-/**
- * Returns a partial Webpack configuration specific to creating a bundle for node
- * @param wco Options which include the build options and app config
- */
-export declare function getServerConfig(wco: WebpackConfigOptions): Configuration;
