51bc8b1cd334d2707e3a7349c5b72dfbceb54659
Juliocotzo <Juliocotzo11@gmail.com>
2020-12-19 20:53:13 -0600

    feat: Funcion alterAddPK

--- parser/team15/TytusDB_G15/report_ts.py
+++ parser/team15/TytusDB_G15/report_ts.py
@@ -1,28 +1,9 @@
-listaErrores = []
-
-class Error:
-    def __init__(self, descripcion: str, linea: int, columna: int, tipo: str):
-        self.descripcion = descripcion
-        self.linea = linea
-        self.columna = columna
-        self.tipo = tipo
-
-class ErrorHTML:
+class RTablaDeSimbolos: 
     def __init__(self):
-        ''' GENERAR HTML PARA LOS ERRORES LEXICOS, SINTACTICOS Y SEMANTICOS'''
+        ''' Reporte Tabla de Simbolos'''
 
-    def getList(self):
-        global listaErrores
-        return listaErrores
-
-    def clearList(self):
-        
-        global listaErrores
-        listaErrores = []
-
-    def crearReporte(self):
-              
-        f = open("reportes/errores.html", "w")
+    def crearReporte(self,ts_global):
+        f = open("reportes/ts.html", "w")
         f.write("<!DOCTYPE html>")
         f.write("<html lang=\"en\" class=\"no-js\">")
         f.write("")
@@ -30,7 +11,7 @@ class ErrorHTML:
         f.write("    <meta charset=\"UTF-8\" />")
         f.write("    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge,chrome=1\">")
         f.write("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">")
-        f.write("    <title>Reporte de Errores</title>")
+        f.write("    <title>Tabla de Simbolos</title>")
         f.write("    <meta name=\"description\"")
         f.write("        content=\"Sticky Table Headers Revisited: Creating functional and flexible sticky table headers\" />")
         f.write("    <meta name=\"keywords\" content=\"Sticky Table Headers Revisited\" />")
@@ -45,31 +26,31 @@ class ErrorHTML:
         f.write("    <div class=\"container\">")
         f.write("        <!-- Top Navigation -->")
         f.write("        <header>")
-        f.write("            <h1>Reporte de Errores</h1>")
+        f.write("            <h1>Tabla de Simbolos</h1>")
         f.write("        </header>")
         f.write("        <div class=\"component\">")
         f.write("            <table>")
         f.write("                <thead>")
         f.write("                    <tr>")
         f.write("                        <th>No.</th>")
-        f.write("                        <th>DESCRIPCION</th>")
-        f.write("                        <th>LINEA</th>")
-        f.write("                        <th>COLUMNA</th>")
-        f.write("                        <th>TIPO ERROR</th>")
+        f.write("                        <th>ID</th>")
+        f.write("                        <th>TIPO</th>")
+        f.write("                        <th>VALOR</th>")
+        f.write("                        <th>AMBITO</th>")
         f.write("                    </tr>")
         f.write("                </thead>")
         f.write("                <tbody>")
-        if listaErrores != []:
+        if len(ts_global.simbolos) > 0:
             i = 0
-            while i < len(listaErrores):
+            while i < len(ts_global.simbolos):
                 f.write("                    <tr>")
                 f.write("                        <td class=\"text-left\">"+ str(i+1) +"</td>")
-                f.write("                        <td class=\"text-left\">"+ str(listaErrores[i].descripcion) +"</td>")
-                f.write("                        <td class=\"text-left\">"+ str(listaErrores[i].linea) +"</td>")
-                f.write("                        <td class=\"text-left\">"+ str(listaErrores[i].columna) +"</td>")
-                f.write("                        <td class=\"text-left\">"+ str(listaErrores[i].tipo) +"</td>")
-                f.write("                    </tr>")   
-                i+=1
+                f.write("                        <td class=\"text-left\">"+ str(ts_global.simbolos[i].id) +"</td>")
+                f.write("                        <td class=\"text-left\">"+ str(ts_global.simbolos[i].tipo) +"</td>")
+                f.write("                        <td class=\"text-left\">"+ str(ts_global.simbolos[i].valor) +"</td>")
+                f.write("                        <td class=\"text-left\">"+ str(ts_global.simbolos[i].ambito) +"</td>")
+                f.write("                    </tr>")
+                i += 1
         f.write("                </tbody>")
         f.write("            </table>")
         f.write("        </div>")
@@ -81,6 +62,3 @@ class ErrorHTML:
         f.write("")
         f.write("</html>")
         f.close()
-        
-        self.clearList()
-
