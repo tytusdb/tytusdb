de915f9ae6f15c9d37ce61b03bbd6a5eb5c3e79a
andremendozatorres <mendozaandre99@gmail.com>
2020-12-21 17:34:24 -0600

    results logic and results display improved

--- parser/team20/execution/generateASTReport.py
+++ parser/team20/execution/generateASTReport.py
@@ -11,24 +11,23 @@ from console import print_error
 def graphAST(self):
         
     dot = "digraph ASTTytus{ \n rankdir = TD\n node[shape = \"box\"]\n"
-    dot += "S[label=\"S\"]\n"
-        
+
+    dotaux = ""
     try:
-        for node in self.nodes:
-            dot += node.graphAST('','S')
-        # archivodot = open("ast.dot", "w")
-        # archivodot.write(dot)
-        # archivodot.close()
-        # cmd = 'dot \"' + 'ast.dot\" -o \"' +  "ast.pdf\" -Tpdf"
-        # print("Comando DOT : " + cmd)
-        # os.system(cmd)
-        # webbrowser.open_new_tab('ast.pdf')
+        if self.nodes != None:
+            for node in self.nodes:
+                dotaux += node.graphAST('','S')
     except Exception as e:
         self.errors.append( Error("Unknown", "Wrong generated AST", 0, 0) )
         print_error("Unknown Error", "Wrong generated AST")
         #print(e)
-        
-    dot += "\n }"        
-    #print(dot)
 
+    if dotaux != "":
+        dot += "S[label=\"S\"]\n"
+        dot += dotaux
+
+    dot += "\n }"
+
+    #print(dot)
+    
     return dot
