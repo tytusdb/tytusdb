9024ec45246e2757cab207db413454af002a321f
Cristian-HP23 <70417026+Cristian-HP23@users.noreply.github.com>
2020-12-20 19:33:06 -0600

    funciones de arbol
    funciones en js

--- client/team06/js/tytus.js
+++ client/team06/js/tytus.js
@@ -1,5 +1,5 @@
 //commentario
-var a;
+
 var arrayCollection = [
   { id: "database", type: "bds", parent: "#", text: "databases" },
   { id: "dog", type: "bd", parent: "database", text: "Dogs" },
@@ -40,3 +40,99 @@ $(function () {
     },
   });
 });
+
+function resfreshJSTree() {
+  $("#tree").jstree(true).settings.core.data = arrayCollection;
+  $("#tree").jstree(true).refresh();
+}
+
+function customMenu(node) {
+  var items = {
+    Create: {
+      label: "New Database",
+      action: function (data) {
+        var ref = $.jstree.reference(data.reference);
+        sel = ref.get_selected();
+        if (!sel.length) {
+          return false;
+        }
+        parentname = sel[0];
+        $("#ModalDB").modal("toggle");
+      },
+    },
+    CreateTT: {
+      label: "New Table",
+      action: function (data) {
+        var ref = $.jstree.reference(data.reference);
+        sel = ref.get_selected();
+        if (!sel.length) {
+          return false;
+        }
+
+        parentname = sel[0];
+        $("#Modaltable").modal("toggle");
+      },
+    },
+  };
+
+  if (node.type === "bd") {
+    delete items.Create;
+  } else if (node.type === "td") {
+    delete items.Create;
+  } else if (node.type === "bds") {
+    delete items.CreateTT;
+  } else {
+    delete items.Create;
+    delete items.CreateTT;
+  }
+
+  return items;
+}
+
+function Guarda() {
+  var porNombre = document.getElementsByName("namedb22")[0].value;
+  nameingresa = porNombre;
+  if (nameingresa !== "" && parentname !== "") {
+    var nuevodb = {
+      id: nameingresa + autoincre,
+      type: "bd",
+      parent: parentname,
+      text: nameingresa,
+    };
+    arrayCollection.push(nuevodb);
+    resfreshJSTree();
+    autoincre++;
+  }
+  nameingresa = "";
+  parentname = "";
+  document.getElementsByName("namedb22")[0].value = "";
+  CierraPopup("#ModalDB");
+  crearBD();
+}
+
+function Guardatb() {
+  var porNombre = document.getElementsByName("nametb2")[0].value;
+  nameingresa = porNombre;
+  if (nameingresa !== "" && parentname !== "") {
+    var nuevotd = {
+      id: nameingresa + autoincre,
+      type: "td",
+      parent: parentname,
+      text: nameingresa,
+    };
+    arrayCollection.push(nuevotd);
+    resfreshJSTree();
+    autoincre++;
+  }
+  nameingresa = "";
+  parentname = "";
+  document.getElementsByName("nametb2")[0].value = "";
+  CierraPopup("#Modaltable");
+  crearTabla();
+}
+
+function CierraPopup(namePop) {
+  $(namePop).modal("hide"); //ocultamos el modal
+  $("body").removeClass("modal-open"); //eliminamos la clase del body para poder hacer scroll
+  $(".modal-backdrop").remove(); //eliminamos el backdrop del modal
+}
