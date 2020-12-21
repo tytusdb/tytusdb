7892c4ea33dca8759ea8b576d3c8f6191b463b25
ByronAlvarez <40155178+ByronAlvarez@users.noreply.github.com>
2020-12-20 19:43:26 -0600

    python server

---
+++ server/team06/main.py
@@ -0,0 +1,12 @@
+import eel
+
+
+eel.init('client')
+
+
+
+eel.start('main.html', size=(1024, 768))
+
+  #let input = document.querySelector("input[name='abrir']");
+  #
+#    let textarea = document.querySelector("textarea[name='query1']");
