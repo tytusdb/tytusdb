5ff7df4a8dfaa3841e61057d065443e4890ace29
José Francisco de Jesús Santos Salazar <71232647+josejfss@users.noreply.github.com>
2020-12-22 16:35:26 -0600

    Actualización de funciones.
    Funciones: 
    * createDatabase
    * showDatabase
    * alterDatabase
    * createDatabase
    * createTable

--- storage/team05/HashMode.py
+++ storage/team05/HashMode.py
@@ -1,1 +1,25 @@
+  
+# Package:      HASH Mode
+# License:      Released under MIT License
+# Notice:       Copyright (c) 2020 TytusDB Team
+# Developers:   Jose Santos, Madelyn Perez, Carlos Campos and Rafael Soliz
 
+import re
+from NameStructure import ne as d 
+from NameStructure import ht as h
+from Archivos import archivo as ar
+
+def createDatabase(database: str):
+    return d.createDatabase(database)
+
+def showDatabases():
+    return d.showDatabases()
+
+def alterDatabase(databaseOld, databaseNew):
+    return d.alterDatabase(databaseOld, databaseNew)
+
+def dropDatabase(database: str):
+    return d.dropDatabase(database)
+
+def createTable(database: str, table: str, numberColumns: int):
+    return d.createTable(database, table, numberColumns)
