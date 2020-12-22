ea082a461fd4fde7ef1f1d8f7fcc200695eec87c
Luis <bachejin@gmail.com>
2020-12-22 17:29:32 -0600

    Seleccion de todo de una o varias tablas
    Impelementados varios cambios para hacer que esta funcionalidad trabaje.

--- parser/team24/main.py
+++ parser/team24/main.py
@@ -3,6 +3,7 @@ import tabla as TabladeSimbolos
 from reportAST import *
 from reportError import *
 from reportBNF import *
+from reportTable import *
 #from graphQueries import graphTree
 default_db = 'DB1'
 ts = TabladeSimbolos.Tabla()
@@ -13,6 +14,10 @@ def meterSimbolos():
     ts.agregar(TabladeSimbolos.Simbolo(2,TabladeSimbolos.TIPO.COLUMN,'id',1,0))
     ts.agregar(TabladeSimbolos.Simbolo(3,TabladeSimbolos.TIPO.COLUMN,'nombre',1,1))
     ts.agregar(TabladeSimbolos.Simbolo(4,TabladeSimbolos.TIPO.COLUMN,'apellido',1,2))
+    ts.agregar(TabladeSimbolos.Simbolo(5,TabladeSimbolos.TIPO.TABLE,'tbusuario',0,None))
+    ts.agregar(TabladeSimbolos.Simbolo(6,TabladeSimbolos.TIPO.COLUMN,'id',5,0))
+    ts.agregar(TabladeSimbolos.Simbolo(7,TabladeSimbolos.TIPO.COLUMN,'nombre',5,1))
+    ts.agregar(TabladeSimbolos.Simbolo(8,TabladeSimbolos.TIPO.COLUMN,'apellido',5,2))
 
 if __name__ == '__main__':
     f = open("./entrada.txt", "r")
@@ -20,16 +25,10 @@ if __name__ == '__main__':
     #print(input)
     meterSimbolos()
     root = g.parse(input)
-    #ejemplo para sacar el arbol
-    executeGraphTree(root)
-    #ejemplo para sacar los errores, cada vez que se ejecuta se limpia la lista al final
-    # es decir que si se ejecuta dos veces seguidas sin ejecuciones de por medio, el segundo reporte estaria vacio
-    report_errors()
-    report_BNF()
 
-    #for val in root:
-        #print(val.ejecutar())
-    #graphTree(root)
+    for val in root:
+        print(val.ejecutar())
+    
 
 
 
