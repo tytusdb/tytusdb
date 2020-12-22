41e218c90e804b126e49012a3207f0984e59ed04
Edgar Ovidio Pérez Jiménez <53838333+eoJimenez@users.noreply.github.com>
2020-12-21 21:17:26 -0600

    agregando clases
    clase nodo y clase lista para almacenar bases de datos

---
+++ storage/team02/DataBaseLista.py
@@ -0,0 +1,21 @@
+import os
+
+class Nodo:
+    def __init__(self, nombreBase):
+        self.nombreBase = nombreBase
+        self.tablas = None
+        self.ldArbol = None
+        self.siguiente = None
+        self.anterior = None
+        
+class ListaDOBLE:
+    def __init__(self):
+        self.primero = None
+        self.ultimo = None
+         
+    
+
+
+
+    
+
