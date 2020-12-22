94d2fc9c9ea26c3d0e0f2e3c2e2f1e8438eec120
ChinJavier <59318465+ChinJavier@users.noreply.github.com>
2020-12-22 11:32:18 -0600

    [G28]UpdateCT

--- parser/team28/models/instructions/DDL/table_inst.py
+++ parser/team28/models/instructions/DDL/table_inst.py
@@ -28,11 +28,21 @@ class CreateTB(Instruction):
         typeChecker = TypeChecker()
         nombreTabla = self._table_name.alias
         noCols = self.numberOfColumns(self._column_list)
-        typeChecker.createTable(nombreTabla,noCols,0,0) #TODO add line and column
+
+        resTab = typeChecker.createTable(nombreTabla,noCols,0,0) #TODO add line and column
+        
+        #Si devuelve None es porque ya existe la tabla
+        if resTab == None:
+            return
+
         # Agrega las propiedades que agrupan a varias columnas
         self.generetaExtraProp()
         # Genera las tablas ya con todas sus propiedades
         self.generateColumns(nombreTabla,typeChecker)
+
+        # Si ocurrio algun error en todo el proceso mato la tabla
+        if self._can_create_flag == False:
+            typeChecker.deleteTable(nombreTabla,0,0)
 
 
     def numberOfColumns(self, arrayColumns):
@@ -50,7 +60,6 @@ class CreateTB(Instruction):
                 self.addUnique(columna._column_list)
 
             elif isinstance(columna,Check):
-                print('Add Check in Table')
                 self.addCheck(columna._column_condition)
 
             elif isinstance(columna,PrimaryKey):
@@ -60,7 +69,6 @@ class CreateTB(Instruction):
                 self.addForeignKey(columna._column_list,columna._table_name,columna._table_column_list)
 
             elif isinstance(columna,Constraint):
-                print('Add Constraint in Table')
                 self.addConstraint(columna._column_name,columna._column_condition)
 
     def generateColumns(self,nombreTabla,typeChecker):
@@ -80,13 +88,22 @@ class CreateTB(Instruction):
                         columnaFinal = self.addPropertyes(prop,columnaFinal)
 
                 tableToInsert = typeChecker.searchTable(SymbolTable().useDatabase, nombreTabla)
-                typeChecker.createColumnTable(tableToInsert,columnaFinal,0,0) #TODO add name of Database, line and column 
-                #print((columnaFinal).__dict__)
+                validateCol = typeChecker.createColumnTable(tableToInsert,columnaFinal,0,0)
+
+                # if return None an error ocurrio
+                if validateCol == None:
+                    self._can_create_flag = False
+                    return
+                
+    
 
     def addPropertyes(self, prop,columnaFinal):
         if prop['default_value'] != None:
-            self.validateType(columnaFinal._dataType,prop['default_value'])
-            #columnaFinal._default = prop['default_value'].value
+            validation = self.validateType(columnaFinal._dataType,prop['default_value'])
+            if validation == None:
+                return
+            else:
+                columnaFinal._default = prop['default_value'].alias
 
         if prop['is_null'] != None:
             if prop['is_null'] == True:
@@ -97,24 +114,29 @@ class CreateTB(Instruction):
                 columnaFinal._notNull = True
 
         if prop['constraint_unique'] != None:
-            columnaFinal._unique = True         #TODO Columna podria tener el atributo constraint unique para darle un alias
+            columnaFinal._unique = True         
 
         if prop['unique'] != None:
             columnaFinal._unique = True
 
         if prop['constraint_check_condition'] != None:
-            columnaFinal._check = True
+            columnaFinal._check =  {
+                                    '_constraint_alias' : None,
+                                    '_condition_check' : prop['constraint_check_condition'].alias
+                                    }
             #prop['constraint_check_condition']  #TODO este es un check que tiene condicional
 
         if prop['check_condition'] != None:
-            columnaFinal._check = True
+            columnaFinal._check = {
+                                    '_constraint_alias' : None,
+                                    '_condition_check' : prop['check_condition'].alias
+                                    }
             #prop['check_condition']  #TODO este es un check que tiene condicional
 
         if prop['pk_option'] != None:
             columnaFinal._primaryKey = True
 
         if prop['fk_references_to'] != None:
-            print(str(prop['fk_references_to']))
             columnaFinal._foreignKey = prop['fk_references_to']
 
         return columnaFinal   
@@ -160,6 +182,7 @@ class CreateTB(Instruction):
     #          foreign key (     ) references id (             ) 
     def addForeignKey(self,listaCols,nombreTabla,listaTablasCols):
         
+        # the len of the cols must be de same
         if len(listaCols) != len(listaTablasCols):
             desc = f": cant of params in foreign() != "
             ErrorController().add(36, 'Execution', desc, 0, 0)
@@ -169,11 +192,20 @@ class CreateTB(Instruction):
         typeChecker = TypeChecker()
         existForeingTable = typeChecker.searchTable(SymbolTable().useDatabase,nombreTabla)
 
+        #validate if the foreign table exists
         if existForeingTable == None:
             desc = f": Undefined table in foreign key ()"
             ErrorController().add(27, 'Execution', desc, 0, 0)
             self._can_create_flag = False
             return
+
+        #validate if the columns exists in the foreign table
+        for coli in listaTablasCols:
+            if typeChecker.searchColumn(existForeingTable,coli) == None:
+                desc = f": Undefined col in table in foreign key ()"
+                ErrorController().add(26, 'Execution', desc, 0, 0)
+                self._can_create_flag = False
+                return
 
         bandera = False
         for x in range(0,len(listaCols)):
@@ -208,7 +240,7 @@ class CreateTB(Instruction):
                             bandera = True
                             columna._properties[0]['constraint_check_condition'] = {
                                                                                 '_constraint_alias' : nombreColumna,
-                                                                                '_condition_check' : True
+                                                                                '_condition_check' : condicionColumna.alias
                                                                                 }
                             break
             if not bandera:
@@ -218,10 +250,41 @@ class CreateTB(Instruction):
                 return
             bandera = False
 
+        else:
+            desc = f": column not given in constraintcheck()"
+            ErrorController().add(26, 'Execution', desc, 0, 0)
+            self._can_create_flag = False
+            return
 
     # Used in CHECK (condition_many_columns)
     def addCheck(self,conditionColumn):
-        pass
+        #                           L (L|D)*
+        whatColumnIs = re.search('[a-zA-z]([a-zA-z]|[0-9])*',conditionColumn.alias)
+        bandera = False
+        if whatColumnIs != None:
+            whatColumnIs = whatColumnIs.group(0)
+            for columna in self._column_list:
+                if isinstance(columna,CreateCol):
+                    if(columna._column_name == whatColumnIs):
+                        if columna._properties != None:
+                            bandera = True
+                            columna._properties[0]['check_condition'] = {
+                                                                                '_constraint_alias' : None,
+                                                                                '_condition_check' : conditionColumn.alias
+                                                                                }
+                            break
+            if not bandera:
+                desc = f": Undefined column in check ()"
+                ErrorController().add(26, 'Execution', desc, 0, 0)
+                self._can_create_flag = False
+                return
+            bandera = False
+
+        else:
+            desc = f": column not given in check()"
+            ErrorController().add(26, 'Execution', desc, 0, 0)
+            self._can_create_flag = False
+            return
 
 
     def validateType(self,columnInfo,defaulValue):
@@ -232,6 +295,7 @@ class CreateTB(Instruction):
 
         valorDef = defaulValue.process(0)
 
+        
         if valorDef != None:
             valorDef = valorDef.value
         else:
@@ -246,9 +310,13 @@ class CreateTB(Instruction):
                 else:
                     desc = f": out of range value in bigint"
                     ErrorController().add(6, 'Execution', desc, 0, 0)
+                    self._can_create_flag = False
+                    return
             except:
                 desc = f": invalid default value to bigint column"
                 ErrorController().add(6, 'Execution', desc, 0, 0)
+                self._can_create_flag = False
+                return
 
         #-->
         elif columnType == 'ColumnsTypes.BOOLEAN':
@@ -257,6 +325,8 @@ class CreateTB(Instruction):
             else:
                 desc = f": invalid default value to boolean column"
                 ErrorController().add(6, 'Execution', desc, 0, 0)
+                self._can_create_flag = False
+                return
 
 
         #-->
@@ -266,11 +336,14 @@ class CreateTB(Instruction):
                 if len(valorDef) > paramOne:
                     desc = f": invalid length in char column, limit is {str(paramOne)}"
                     ErrorController().add(6, 'Execution', desc, 0, 0)
+                    self._can_create_flag = False
+                    return
             else:
                 if len(valorDef) > 1:
                     desc = f": invalid length in char column, limit is 1"
                     ErrorController().add(6, 'Execution', desc, 0, 0)
-                    
+                    self._can_create_flag = False
+                    return
 
         #-->
         elif columnType == 'ColumnsTypes.CHARACTER':
@@ -279,10 +352,14 @@ class CreateTB(Instruction):
                 if len(valorDef) > paramOne:
                     desc = f": invalid len in character column, limit is {str(paramOne)}"
                     ErrorController().add(6, 'Execution', desc, 0, 0)
+                    self._can_create_flag = False
+                    return
             else:
                 if len(valorDef) > 1:
                     desc = f": invalid length in char column, limit is 1"
                     ErrorController().add(6, 'Execution', desc, 0, 0)
+                    self._can_create_flag = False
+                    return
 
         #-->
         elif columnType == 'ColumnsTypes.CHARACTER_VARYING':
@@ -291,6 +368,8 @@ class CreateTB(Instruction):
                 if len(valorDef) > paramOne:
                     desc = f": invalid len in varying column, limit is {str(paramOne)}"
                     ErrorController().add(6, 'Execution', desc, 0, 0)
+                    self._can_create_flag = False
+                    return
             else:
                 pass
 
@@ -302,6 +381,8 @@ class CreateTB(Instruction):
             except:
                 desc = f": invalid format in date column"
                 ErrorController().add(17, 'Execution', desc, 0, 0)
+                self._can_create_flag = False
+                return
 
         #-->
         elif columnType == 'ColumnsTypes.DECIMAL' or columnType == 'ColumnsTypes.NUMERIC':
@@ -320,6 +401,8 @@ class CreateTB(Instruction):
                         if conteo > paramOne:
                             desc = f": overflow in decimal or numeric column"
                             ErrorController().add(6, 'Execution', desc, 0, 0)
+                            self._can_create_flag = False
+                            return
                 else:
                     strValorDef = str(valorDef)
                     if strValorDef.find('.') != -1:
@@ -328,10 +411,14 @@ class CreateTB(Instruction):
                         if len(str(parteEntera)) > paramOne and parteEntera != 0:
                             desc = f": overflow in decimal or numeric column"
                             ErrorController().add(6, 'Execution', desc, 0, 0)
+                            self._can_create_flag = False
+                            return
 
             except:
                 desc = f": invalid default value in decimal or numeric"
                 ErrorController().add(6, 'Execution', desc, 0, 0)
+                self._can_create_flag = False
+                return
 
         elif columnType == 'ColumnsTypes.DOUBLE_PRECISION' or columnType == 'ColumnsTypes.REAL':
             try:
@@ -339,6 +426,8 @@ class CreateTB(Instruction):
             except:
                 desc = f": invalid default value in double or real"
                 ErrorController().add(6, 'Execution', desc, 0, 0)
+                self._can_create_flag = False
+                return
 
         #-->
         elif columnType == 'ColumnsTypes.INTEGER':
@@ -349,9 +438,13 @@ class CreateTB(Instruction):
                 else:
                     desc = f": default value out of range in integer col"
                     ErrorController().add(6, 'Execution', desc, 0, 0)
+                    self._can_create_flag = False
+                    return
             except:
                 desc = f": invalid default value in integer col"
                 ErrorController().add(6, 'Execution', desc, 0, 0)
+                self._can_create_flag = False
+                return
 
         #-->
         elif columnType == 'ColumnsTypes.INTERVAL':
@@ -364,6 +457,8 @@ class CreateTB(Instruction):
             except:
                 desc = f": invalid default value in money col"
                 ErrorController().add(6, 'Execution', desc, 0, 0)
+                self._can_create_flag = False
+                return
 
         #-->
         elif columnType == 'ColumnsTypes.SMALLINT':
@@ -374,9 +469,13 @@ class CreateTB(Instruction):
                 else:
                     desc = f": default value out of range in small col"
                     ErrorController().add(6, 'Execution', desc, 0, 0)
+                    self._can_create_flag = False
+                    return
             except:
                 desc = f": invalid default value in smallint col"
                 ErrorController().add(6, 'Execution', desc, 0, 0)
+                self._can_create_flag = False
+                return
         
         #-->
         elif columnType == 'ColumnsTypes.TEXT':
@@ -385,6 +484,8 @@ class CreateTB(Instruction):
             except:
                 desc = f": invalid default value in text col"
                 ErrorController().add(6, 'Execution', desc, 0, 0)
+                self._can_create_flag = False
+                return
         
         #-->
         elif columnType == 'ColumnsTypes.TIMESTAMP':
@@ -394,6 +495,8 @@ class CreateTB(Instruction):
             except:
                 desc = f": invalid format timpestamp, yyyy-mm-dd h:m:s"
                 ErrorController().add(17, 'Execution', desc, 0, 0)
+                self._can_create_flag = False
+                return
         
         #-->
         elif columnType == 'ColumnsTypes.TIME':
@@ -403,6 +506,8 @@ class CreateTB(Instruction):
             except:
                 desc = f": invalid format time, h:m:s"
                 ErrorController().add(17, 'Execution', desc, 0, 0)
+                self._can_create_flag = False
+                return
 
         elif columnType == 'ColumnsTypes.VARCHAR':
             valorDef = str(valorDef)
@@ -410,8 +515,13 @@ class CreateTB(Instruction):
                 if len(valorDef) > paramOne:
                     desc = f":  out of range value in varchar col"
                     ErrorController().add(6, 'Execution', desc, 0, 0)
+                    self._can_create_flag = False
+                    return
             else:
                 pass
+        #Si llega a este punto todo piola
+        return True        
+
 
 class DropTB(Instruction):
 
