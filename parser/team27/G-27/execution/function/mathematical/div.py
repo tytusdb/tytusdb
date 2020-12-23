e8a872777235fbe751289dc202ca725a0e784f45
José Rafael Morente González <josemorenteg98@outlook.com>
2020-12-23 03:15:33 -0600

    [FIXED] Arreglado importes de funciones matemáticas, trigonometricas, binarias y de agregado.
    [FIXED] Arreglado importes de funciones matemáticas, trigonometricas, binarias y de agregado.
    +.......

--- parser/team27/G-27/execution/function/mathematical/div.py
+++ parser/team27/G-27/execution/function/mathematical/div.py
@@ -1,11 +1,6 @@
-import sys
-sys.path.append('../tytus/parser/team27/G-27/execution/abstract')
-sys.path.append('../tytus/parser/team27/G-27/execution/expression')
-sys.path.append('../tytus/parser/team27/G-27/execution/symbol')
-sys.path.append('../tytus/parser/team27/G-27/libraries')
-from function import *
-from typ import *
-from math_functions import div
+from execution.abstract.function import *
+from execution.symbol.typ import *
+from libraries.math_functions import div
 
 class Div(Function):
     def __init__(self, divisor, dividend, row, column):
