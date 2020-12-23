0f8dfaa7bfe7700d238cf37839ccd8629b52019e
Luis Arana <luarana631@gmail.com>
2020-12-22 18:37:19 -0600

    Grupo 23 - 201700988 - Funciones de Tablas

--- parser/team23/abstract/retorno.py
+++ parser/team23/abstract/retorno.py
@@ -1,5 +1,6 @@
 class retorno:
-    def __init__(self, valor, tipo, query=False):
+    def __init__(self, valor, tipo, query=False, index_col = None):
         self.valor = valor
         self.tipo = tipo
         self.query = query
+        self.index_col = index_col
