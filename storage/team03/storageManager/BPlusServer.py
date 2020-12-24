f84961d1b7bc9ccaf5784cb28ab9ee1504ba4bcb
Herberth Avila <ibiem3@gmail.com>
2020-12-23 23:03:10 -0600

    Metodos necesarios para la funcion del servidor

--- storage/team03/storageManager/BPlusServer.py
+++ storage/team03/storageManager/BPlusServer.py
@@ -243,4 +243,168 @@ class BPlusServer:
         #Se pone en None la raiz de la estructura B+ de la tabla especificada
         tableBuscada.objeto.estructura.truncateRaiz()
 
-    
+    #############
+    # Utilities #
+    #############
+
+    def __getNombreASCII(self, cadena):
+        number = 0
+        for c in cadena:
+            number += ord(c)
+        
+        return number
+
+    #Metodo unicamente para utilizar en la interfaz grafica
+    def generarReporteDB(self):
+        #Crea el archivo .dot y la imagen .png del arbol de las base de datos (Arbol AVL)
+        self.__db_Tree.reporteDB()
+
+        #Retorna la lista de nombres de las base de datos
+        return self.__db_Tree.getListaNombres()
+
+    #Metodo unicamente para utilizar en la interfaz grafica
+    def generarReporteTabla(self, database):
+        #Se extrae el nodo de la base de datos buscada
+        idDB = self.__getNombreASCII(database)
+        dbBuscada = self.__db_Tree.buscarObjeto(idDB, database)
+        #Crea el archivo .dot y la imagen .png del arbol de las tablas (Arbol AVL)
+        dbBuscada.objeto.estructura.reporteTablas()
+
+        #Retorna la lista de nombres de las tablas
+        return dbBuscada.objeto.estructura.getListaNombres()
+
+    #Metodo unicamente para utilizar en la interfaz grafica
+    def generarReporteBMasPlus(self, database, table):
+        #Se extrae el nodo de la base de datos buscada
+        idDB = self.__getNombreASCII(database)
+        dbBuscada = self.__db_Tree.buscarObjeto(idDB, database)
+        #Se extrae el nodo de la tabla buscada
+        idTable = self.__getNombreASCII(table)
+        tableBuscada = dbBuscada.objeto.estructura.buscarObjeto(idTable, table)
+        #Crea el archivo .dot y la imagen .png del arbol de las tuplas (Arbol B+)
+        tableBuscada.objeto.estructura.graficar()
+
+    def existeDB(self, database):
+        existe = True
+        #Buscar y trae el nodo con de la DB
+        idDB = self.__getNombreASCII(database)
+        dbBuscada = self.__db_Tree.buscarObjeto(idDB, database)
+        #Si la base de datos no existe, el atributo existe = False
+        if dbBuscada is None:
+            existe = False
+
+        return existe
+
+    def existeTabla(self, database, table):
+        existe = True
+        #Buscar el nodo con de la DB y retornarlo
+        idDB = self.__getNombreASCII(database)
+        dbBuscada = self.__db_Tree.buscarObjeto(idDB, database)
+        #Se extrae el nodo de la tabla buscada
+        idTable = self.__getNombreASCII(table)
+        tableBuscada = dbBuscada.objeto.estructura.buscarObjeto(idTable, table)
+        #Si la tabla no existe, el atributo existe = False
+        if tableBuscada is None:
+            existe = False
+
+        return existe
+
+    def __commit(self, objeto, nombre):
+        file = open(nombre+".bin","wb+")
+        file.write(pickle.dumps(objeto))
+        file.close()
+
+    def __rollback(self, nombre):
+        file = open(nombre+".bin", "rb")
+        b = file.read()
+        file.close()
+        return pickle.loads(b)
+
+    def serializar(self):
+        #Se recorre el arbol de BD y se retorna una lista con los objetos BD
+        listaDb = self.__db_Tree.getObjetosList()
+        directorioDataBases = []
+
+        for db in listaDb:
+            #Se crea una lista vacia que recolectara las rutas de las tablas que contiene la DB especifica
+            directorioTablas = []
+            #Se captura el nombre de la base de datos actual
+            nombreDb = db.nombre
+            #Se recorre el arbol de Tablas y se retorna una lista con los objetos Tabla de la BD especifica
+            listaTablas = db.estructura.getObjetosList()
+
+            for t in listaTablas:
+                #Se captura el nombre de la tabla actual
+                nombreTablaActual = t.nombre
+                #Se capturan los atributos que contenia la estructura B+ para las tuplas (como validaciones iniciales)
+                pk = t.estructura.pk
+                auto = t.estructura.auto
+                tamCol = t.estructura.tamCol
+
+                #Se listan los atributos para validacion de la estructura B+
+                listaAtributos = [nombreTablaActual, pk, auto, tamCol]
+                #Lista que contiene las tuplas (lista de atributos)
+                listaTuplas = t.estructura.tabla.ListaEnlazada(None, None, None)
+                #Para el almacenaje a disco se hace una lista que contiene los atributos de validacion y
+                #las tuplas de una tabla en especifico
+                listaRegistro = [listaAtributos, listaTuplas]
+
+                #Se crea el archivo binario de la tabla actual que contendra serializado la listaRegistro
+                self.__commit(listaRegistro, "Tablas/"+nombreTablaActual)
+                #Se recolectan las rutas de los archivos binarios de las tablas en la base de datos especifica
+                directorioTablas.append("Tablas/"+nombreTablaActual) 
+            
+            #Se crea el archivo binario de la base de datos actual que contendra serializado los directorios de sus tablas
+            self.__commit(directorioTablas, "Base_De_Datos/"+nombreDb)
+            #Se recolectan las rutas de los archivos binarios de las bases de datos para guardarlos en el archivo 'data'
+            directorioDataBases.append("Base_De_Datos/"+nombreDb)
+
+        #Se crea el archivo binario 'data' que contendra serializado los directorios de las bases de datos recolectada
+        self.__commit(directorioDataBases, "data")
+
+    def deserializar(self):
+        #Si el archivo binario 'data' no existe no hay nada para deserializar
+        if os.path.exists("data.bin"):
+            #Deserializa el archivo binario 'data' que retorna una lista de rutas para las bases de datos
+            directoriosDataBases = self.__rollback("data")
+            #Se recorre cada base de datos obteniendola por su ruta de archivo
+            for directDb in directoriosDataBases:
+                #Capturamos el nombre de la base de datos
+                nombreDb = directDb.replace("Base_De_Datos/", "")
+                #Se crea la base de datos para tenerla en memoria
+                self.createDB(nombreDb)
+                #Deserializa el archivo binario de la base de datos actual que rotorna una lista de rutas de las tablas dentro de la BD
+                directorioTablas = self.__rollback(directDb)
+            
+                #Se recorre cada tabla obteniendola por su ruta de archivo
+                for directT in directorioTablas:
+                    #Se extrae la lista de registros que almacena cada archivo de tabla
+                    listaRegistro = self.__rollback(directT)
+                    #La lista de registros extraida contiene la lista de atributos para actualizar validaciones
+                    #en las tuplas que se van a insertar
+                    listaAtributos = listaRegistro[0]
+                    #Se extrae la lista de tuplas contenida en la lista registros
+                    listaTuplas = listaRegistro[1]
+
+                    #Se crea una tabla en donde listaAtributos[0] señala el nombre de la tabla
+                    #y listaAtributos[1] señala el numero de columnas que debe tener sus tuplas
+                    self.createT(nombreDb, listaAtributos[0], listaAtributos[3])
+
+                    #Buscar el nodo con de la DB y retornarlo
+                    idDB = self.__getNombreASCII(nombreDb)
+                    dbBuscada = self.__db_Tree.buscarObjeto(idDB, nombreDb)
+
+                    #Se extrae el nodo de la tabla buscada
+                    idTable = self.__getNombreASCII(listaAtributos[0])
+                    tableBuscada = dbBuscada.objeto.estructura.buscarObjeto(idTable, listaAtributos[0])
+
+                    #Ya con la tabla creada se vuelve a llamar para actualizar los atributos de
+                    #la estructura B+ contenida en cada objeto Tabla
+                    tableBuscada.objeto.estructura.pk = listaAtributos[1]
+                    tableBuscada.objeto.estructura.auto = listaAtributos[2]
+                    tableBuscada.objeto.estructura.tamCol = listaAtributos[3]
+
+                    #Se recorre cada tupla para insertarla en su base de datos
+                    #y tabla respectiva. listaAtributos[0] señala el nombre de la tabla
+                    for tupla in listaTuplas:
+                        self.insert(nombreDb, listaAtributos[0], tupla)
