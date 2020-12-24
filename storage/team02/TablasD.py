a350d5e9f5e4aef63a014dba73cecab0378581c4
Jorge Argueta <34260059+Arg45@users.noreply.github.com>
2020-12-23 22:22:03 -0600

    3. funcion1
    funcion 1, crear tabla

--- storage/team02/TablasD.py
+++ storage/team02/TablasD.py
@@ -14,3 +14,28 @@ class TablasArboles:
         for i in range(len(tabla.elementosAB.listRegister)):
             del tabla.elementosAB.listRegister[i].register[num]
         return True
+        
+#Funcion 1 - crear tabla
+# def createTable(database: str, table: str, numberColumns: int) -> int:    
+    def createT(self,database,table,numberColumns) :
+        #print(e)
+        
+        bdEncontrada=self.bd.buscarNodo(database)
+        if bdEncontrada != None and bdEncontrada != 0:      #se agrego la 2da condicion por si retorna algun numero                                       
+            if bdEncontrada.tablas == None :
+                bdEncontrada.tablas = ListaDobledeArboles()
+            if bdEncontrada.tablas.buscar(table) == None :
+                #BD encontrada.tablas=table
+                bdEncontrada.tablas.insertar(table,numberColumns)
+                if bdEncontrada.tablas != None :
+                    #return ("Operacion exitosa")
+                    return (0)
+                else:
+                    #return ("Error en la operacion")
+                    return (1)
+            else:
+                #return ("Tabla existente")
+                return (3)
+        else:                                                                      
+            #return ("BD inexistente")
+            return (2)  
