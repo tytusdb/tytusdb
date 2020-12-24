576f5226063c0315ad5c2e4d8df2474ff1db917c
Jorge Argueta <34260059+Arg45@users.noreply.github.com>
2020-12-23 22:19:09 -0600

    1. agregar columna
    submetodo de crear columna

--- storage/team02/TablasD.py
+++ storage/team02/TablasD.py
@@ -4,3 +4,8 @@ from Tablas import *
 class TablasArboles:  
     def __init__(self,bd) :
         self.bd=bd
+
+    def agregarColumna(self,val,tabla) :
+        for i in range(len(tabla.elementosAB.listRegister)) :
+            tabla.elementosAB.listRegister[i].register.append(val)
+        return True
