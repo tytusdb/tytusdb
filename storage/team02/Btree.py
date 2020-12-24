b62abd13725a72d6f4df6b9d26e1e6d906350b59
Steve <briansmor09@gmail.com>
2020-12-23 23:36:41 -0600

    Clase 2

--- storage/team02/Btree.py
+++ storage/team02/Btree.py
@@ -6,3 +6,16 @@ class NodeBTree :
         self.keys = []
         self.children = []
         self.parentNode = None
+class Registro:
+    def __init__(self,listRegister):
+        self.register = listRegister
+        self.idHide = 0
+
+class BTree :
+    
+    def __init__(self):
+        self.root = None
+        self.identity = 0
+        self.maxColumna = 0
+        self.listRegister = []
+        self.columPK = [0]
