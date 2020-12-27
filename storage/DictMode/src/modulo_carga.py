46059889702a67cae4593d8ecaee82a890a4a23e
Andree <aavalosoto@gmail.com>
2020-12-26 20:21:30 -0600

    DictMode
    Modo empleado con diccionarios como estructuras para el modo de almacenamiento

---
+++ storage/DictMode/src/modulo_carga.py
@@ -0,0 +1,18 @@
+# License:      Released under MIT License
+# Notice:       Copyright (c) 2020 TytusDB Team
+# Developer:    Andree Avalos
+import sys
+sys.path.append('./src')
+from modulo_llaves import insert
+import csv
+def loadCSV(filepath: str, database: str, table: str):
+    try:
+        res = []
+        import csv
+        with open(filepath, 'r') as file:
+            reader = csv.reader(file, delimiter = ',')
+            for row in reader:
+                res.append(insert(database,table, row))
+        return res
+    except:
+        return []
