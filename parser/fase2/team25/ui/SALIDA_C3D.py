32793007ba59de465ff743a6d882cce89295aae2
PabloAndresArg <PabloAndresArg@gmail.com>
2021-01-03 18:42:02 -0600

    interfaz con codigo 3d

---
+++ parser/fase2/team25/ui/SALIDA_C3D.py
@@ -0,0 +1,21 @@
+from goto import with_goto
+from interpreter import execution
+from c3d.stack import  Stack
+
+stack = Stack()
+
+
+@with_goto
+def principal():
+	t0 = "use test;"
+	stack.push(t0)
+	funcionIntermedia()
+	t1 = "insert into nacha values (3,45,6);"
+	stack.push(t1)
+	funcionIntermedia()
+
+
+
+def funcionIntermedia():
+	execution(stack.pop())
+principal()
