d5026c74213db402d68b97b76a063fcbfb8aceb3
CarlosCante <carloscante@gmail.com>
2021-01-05 16:55:05 -0600

    Traduccion de INSERT

--- parser/fase2/team07/Tytus_SQLPARSER_G8/sintactico.py
+++ parser/fase2/team07/Tytus_SQLPARSER_G8/sintactico.py
@@ -25,7 +25,7 @@ from Instrucciones.Sql_select import GroupBy, Having, Limit, OrderBy, Select, Wh
 from Instrucciones.Sql_truncate import Truncate
 from Instrucciones.Sql_update import UpdateTable
 from Instrucciones.Sql_create import Columna as CColumna
-from Instrucciones import Relaciones
+from Instrucciones import Relaciones, LlamadoFuncion
 
 from Instrucciones.plpgsql import condicional_if 
 
@@ -713,8 +713,6 @@ def p_instruccion_select2(t):
     strGram = "<query> ::= SELECT <dist> <lcol> FROM <lcol> <instructionWhere>"
     strGram2 = ""
     val = []
-    val.append(Select.Select(t[2], t[3], t[5], None, t[6], None, strGram,t.lexer.lineno, t.lexer.lexpos))
-    
     strSent = "SELECT " + t[2]
     for col in t[3]:
         if isinstance(col, str):
@@ -730,6 +728,10 @@ def p_instruccion_select2(t):
             strSent = strSent + col.strSent + ","
     strSent = strSent[:-1]
     strSent = strSent + " " + t[6].strSent
+
+    val.append(Select.Select(t[2], t[3], t[5], None, t[6], None, strGram,t.lexer.lineno, t.lexer.lexpos, strSent))
+    
+    
 
 
     t[0] = SelectLista.SelectLista(val, strGram2, t.lexer.lineno, t.lexer.lexpos, strSent)
@@ -976,26 +978,25 @@ def p_operadores_relacionales(t):
     '''
     strGram = ""
     strSent = ""
-    if t[2] == "IGUAL":
+    if t[2] == "=":
         strGram = "<expre> ::= <expre> IGUAL <expre>"
         strSent = t[1].strSent + " = " + t[3].strSent
-    elif t[2] == "MAYORQ":
+    elif t[2] == ">":
         strGram = "<expre> ::= <expre> MAYORQ <expre>"
         strSent = t[1].strSent + " > " + t[3].strSent
-    elif t[2] == "MENORQ":
+    elif t[2] == "<":
         strGram = "<expre> ::= <expre> MENORQ <expre>"
         strSent = t[1].strSent + " < " + t[3].strSent
-    elif t[2] == "MAYOR_IGUALQ":
+    elif t[2] == ">=":
         strGram = "<expre> ::= <expre> MAYOR_IGUALQ <expre>"
         strSent = t[1].strSent + " >= " + t[3].strSent
-    elif t[2] == "MENOR_IGUALQ":
+    elif t[2] == "<=":
         strGram = "<expre> ::= <expre> MENOR_IGUALQ <expre>"
         strSent = t[1].strSent + " <= " + t[3].strSent
-    elif t[2] == "DISTINTO":
+    elif t[2] == "<>":
         strGram = "<expre> ::= <expre> DISTINTO <expre>"
         strSent = t[1].strSent + " <> " + t[3].strSent
 
-    
     t[0] = Relacional.Relacional(t[1], t[3], t[2],strGram ,t.lexer.lineno, t.lexer.lexpos, strSent)
 
 def p_operadores_aritmeticos(t):
@@ -1009,22 +1010,22 @@ def p_operadores_aritmeticos(t):
     
     strGram = ""
     strSent = ""
-    if t[2] == "MAS":
+    if t[2] == "+":
         strGram = "<expre> ::= <expre> MAS <expre>"
         strSent = t[1].strSent + " + " + t[3].strSent
-    elif t[2] == "MENOS":
+    elif t[2] == "-":
         strGram = "<expre> ::= <expre> MENOS <expre>"
         strSent = t[1].strSent + " - " + t[3].strSent
-    elif t[2] == "POR":
+    elif t[2] == "*":
         strGram = "<expre> ::= <expre> POR <expre>"
         strSent = t[1].strSent + " * " + t[3].strSent
-    elif t[2] == "DIVIDIDO":
+    elif t[2] == "/":
         strGram = "<expre> ::= <expre> DIVIDIDO <expre>"
         strSent = t[1].strSent + " / " + t[3].strSent
-    elif t[2] == "EXPONENCIACION":
+    elif t[2] == "^":
         strGram = "<expre> ::= <expre> EXPONENCIACION <expre>"
         strSent = t[1].strSent + " ^ " + t[3].strSent
-    elif t[2] == "MODULO":
+    elif t[2] == "%":
         strGram = "<expre> ::= <expre> MODULO <expre>"
         strSent = t[1].strSent + " % " + t[3].strSent
 
@@ -1829,6 +1830,16 @@ def p_expresion7(t):
 def p_expresion8(t):
     '''expresion : ID PARIZQ lcol PARDER
     '''
+    strGram = "<expresion> ::= ID PARIZQ <lcol> PARDER\n"
+    strSent = t[1] + "("
+    for col in t[3]:
+        if isinstance(col, str):
+            strSent = strSent + col + ","
+        else:
+            strSent = strSent + col.strSent + ","
+    strSent = strSent[:-1]
+    strSent = strSent + ")"
+    t[0] = LlamadoFuncion.LlamadoFuncion(strGram, t.lexer.lineno, t.lexer.lexpos, strSent)
 
 def p_expresion9(t):
     '''expresion : TRUE
@@ -2349,6 +2360,10 @@ def p_index_first_last_e(t):
     '''
     first_last_op : 
     '''
+
+
+
+
 
 #FIN DE LA GRAMATICA
 # MODO PANICO ***************************************
