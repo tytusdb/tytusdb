f8de9014116470e9bc53259aeec7410ce3e34311
LopDlMa <dulcita11@gmail.com>
2021-01-04 23:29:29 -0600

    feat: Instrucciones y Expresiones PLSQL
    feat: Instrucciones y Expresiones PLSQL

--- parser/fase2/team15/TytusDB_G15/PLSQL/gramaticaPLSQL.py
+++ parser/fase2/team15/TytusDB_G15/PLSQL/gramaticaPLSQL.py
@@ -183,6 +183,7 @@ def t_newline(t):
 # Error Lexico
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
+    listaErroresLexicos.append(ErrorLexico(t.value[0], t.lexer.lineno, t.lexpos))
     t.lexer.skip(1)
 
 # Construyendo el analizador léxico
@@ -206,34 +207,45 @@ precedence = (
 
 
 # Importacion de clases para la creación del AST
+from PLSQL.expresionesPLSQL import *
+from PLSQL.instruccionesPLSQL import *
 
 # Definición de la gramática ---------------------------------------------------------------------------------------------------
 listaGramatica = []
 
 def p_inicio(t):
     'inicio    : codigo'
+    t[0] = t[1]
 
 def p_lenguaje_augus(t):
     '''codigo    : instrucciones_globales_list'''
+    t[0] = t[1]
 
 def p_instrucciones_globales_list(t):
     'instrucciones_globales_list    : instrucciones_globales_list instrucciones_global_sent'
+    t[1].append(t[2])
+    t[0] = t[1]
 
 def p_instrucciones_globales_list_sent(t):
     'instrucciones_globales_list    : instrucciones_global_sent'
+    t[0] = [t[1]]
 
 def p_instrucciones_global_sent(t):
     '''instrucciones_global_sent    : funcion
                                     | llamada_funcion'''
+    t[0] = t[1]
 
 def p_instrucciones_global_sent_error(t):
     'instrucciones_global_sent    : error'
 
 def p_instrucciones_funct_list(t):
     'instrucciones_funct_list    : instrucciones_funct_list instrucciones_funct_sent'
+    t[1].append(t[2])
+    t[0] = t[1]
 
 def p_instrucciones_funct_list_sent(t):
     'instrucciones_funct_list    : instrucciones_funct_sent'
+    t[0] = [t[1]]
 
 def p_instrucciones_funct_sent(t):
     '''instrucciones_funct_sent    : asignacion
@@ -244,110 +256,170 @@ def p_instrucciones_funct_sent(t):
                                     | PUNTOYCOMA
                                     | llamada_funcion
                                     | empty'''
+    t[0] = t[1]
 
 def p_instrucciones_funct_sent_error(t):
     'instrucciones_funct_sent    : error'
 
 def p_funcion(t):
     'funcion    : CREATE FUNCTION ID PARA parametros PARC RETURNS tipo AS DOLAR DOLAR BEGIN instrucciones_funct_list END PUNTOYCOMA DOLAR DOLAR LANGUAGE PLPGSQL PUNTOYCOMA'
+    t[0] = Funcion(TIPO_DATO.INT, t[3], t[5], Principal(t[13]))
 
 def p_funcion2(t):
     'funcion    : CREATE FUNCTION ID PARA parametros PARC RETURNS tipo AS DOLAR DOLAR DECLARE instrucciones_funct_list BEGIN instrucciones_funct_list END PUNTOYCOMA DOLAR DOLAR LANGUAGE PLPGSQL PUNTOYCOMA'
-    
+    instrucs = []
+    for instru1 in t[13]:
+        instrucs.append(instru1)
+    for instru2 in t[15]:
+        instrucs.append(instru2)
+    t[0] = Funcion(TIPO_DATO.INT, t[3], t[5], Principal(instrucs))
+
 def p_funcion_r(t):
     'funcion    : CREATE OR REPLACE FUNCTION ID PARA parametros PARC RETURNS tipo AS DOLAR DOLAR BEGIN instrucciones_funct_list END PUNTOYCOMA DOLAR DOLAR LANGUAGE PLPGSQL PUNTOYCOMA'
+    t[0] = Funcion(TIPO_DATO.INT, t[5], t[7], Principal(t[15]))
 
 def p_funcion2_r(t):
     'funcion    : CREATE OR REPLACE FUNCTION ID PARA parametros PARC RETURNS tipo AS DOLAR DOLAR DECLARE instrucciones_funct_list BEGIN instrucciones_funct_list END PUNTOYCOMA DOLAR DOLAR LANGUAGE PLPGSQL PUNTOYCOMA'
-    
+    instrucs = []
+    for instru1 in t[15]:
+        instrucs.append(instru1)
+    for instru2 in t[17]:
+        instrucs.append(instru2)
+    t[0] = Funcion(TIPO_DATO.INT, t[5], t[7], Principal(instrucs))
+
 #PROCEDURE
 def p_procedure(t):
     'funcion    : CREATE PROCEDURE ID PARA parametros PARC RETURNS tipo AS DOLAR DOLAR BEGIN instrucciones_funct_list END PUNTOYCOMA DOLAR DOLAR LANGUAGE PLPGSQL PUNTOYCOMA'
+    t[0] = Funcion(TIPO_DATO.INT, t[3], t[5], Principal(t[13]))
 
 def p_procedure2(t):
     'funcion    : CREATE PROCEDURE ID PARA parametros PARC RETURNS tipo AS DOLAR DOLAR DECLARE instrucciones_funct_list BEGIN instrucciones_funct_list END PUNTOYCOMA DOLAR DOLAR LANGUAGE PLPGSQL PUNTOYCOMA'
-    
+    instrucs = []
+    for instru1 in t[13]:
+        instrucs.append(instru1)
+    for instru2 in t[15]:
+        instrucs.append(instru2)
+    t[0] = Funcion(TIPO_DATO.INT, t[3], t[5], Principal(instrucs))
+
 def p_procedure_r(t):
     'funcion    : CREATE OR REPLACE PROCEDURE ID PARA parametros PARC RETURNS tipo AS DOLAR DOLAR BEGIN instrucciones_funct_list END PUNTOYCOMA DOLAR DOLAR LANGUAGE PLPGSQL PUNTOYCOMA'
-  
+    t[0] = Funcion(TIPO_DATO.INT, t[5], t[7], Principal(t[15]))
+
 def p_procedure2_r(t):
     'funcion    : CREATE OR REPLACE PROCEDURE ID PARA parametros PARC RETURNS tipo AS DOLAR DOLAR DECLARE instrucciones_funct_list BEGIN instrucciones_funct_list END PUNTOYCOMA DOLAR DOLAR LANGUAGE PLPGSQL PUNTOYCOMA'
-   
+    instrucs = []
+    for instru1 in t[15]:
+        instrucs.append(instru1)
+    for instru2 in t[17]:
+        instrucs.append(instru2)
+    t[0] = Funcion(TIPO_DATO.INT, t[5], t[7], Principal(instrucs))
+    
+
 def p_llamada_funcion(t):
     'llamada_funcion    : SELECT ID PARA params PARC PUNTOYCOMA'
-  
+    t[0] = LlamadaFuncion(t[2], t[4])
+
 def p_llamada_funcion1(t):
     'llamada_funcion    : CALL ID PARA params PARC PUNTOYCOMA'
-   
+    t[0] = LlamadaFuncion(t[2], t[4])
+
 def p_params_list(t):
     'params     : params COMA expresion'
-  
+    t[1].append(t[3])
+    t[0] = t[1]
 
 def p_params_sent(t):
     '''params   : expresion
                 | empty'''
+    t[0] = [t[1]]
 
 def p_parametros_list(t):
     'parametros     : parametros COMA parametro'
-
+    t[1].append(t[3])
+    t[0] = t[1]
 
 def p_parametros_sent(t):
     'parametros     : parametro'
+    t[0] = [t[1]]
+
 
 def p_parametro1(t):
     'parametro       : ID tipo'
+    t[0] = Parametro(t[2], t[1])
 
 def p_parametro2(t):
     '''parametro       : empty'''
+    t[0] = None
 
 def p_sentencia_switch(t):
     'sentencia_switch   : CASE expresion case_list END CASE PUNTOYCOMA'
+    t[0] = SentenciaCase(t[2], t[3])
 
 def p_case_list_list(t):
     '''case_list    : case_list case'''
+    t[1].append(t[2])
+    t[0] = t[1]
 
 def p_case_list_sent(t):
     '''case_list    : case'''
+    t[0] = [t[1]]
 
 def  p_case(t):
     '''case     : WHEN expresion THEN instrucciones_funct_list'''
+    t[0] = Caso(t[2], Principal(t[4]))
 
 def  p_case_default(t):
     '''case     : ELSE instrucciones_funct_list'''
+    t[0] = Caso(None, Principal(t[2]))
 
 def p_sentencia_if(t):
     'sentencia_if   : IF expresion THEN instrucciones_funct_list else END IF PUNTOYCOMA'
+    t[0] = SentenciaIf(t[2], Principal(t[4]), t[5])
 
 def p_sentencia_if_else1(t):
     'else     : ELSE instrucciones_funct_list '
+    t[0] = Principal(t[2])
+
 def p_sentencia_if_else2(t):
     'else     : ELSEIF expresion THEN instrucciones_funct_list else '
+    t[0] = SentenciaIf(t[2], Principal(t[4]), t[5])
 
 def p_sentencia_if_else3(t):
     'else     : '
+    t[0] = None
 
 def p_imprimir(t):
     'imprimir   : RAISE lista_imprimir PUNTOYCOMA'
+    t[0] = Impresion(t[2])
 
 def p_imprimir_lista(t):
     'lista_imprimir     : lista_imprimir COMA sent_imprimir'
+    t[1].append(t[3])
+    t[0] = t[1]
 
 def p_imprimir_lista_sent(t):
     'lista_imprimir     : sent_imprimir'
+    t[0] = [t[1]]
 
 def p_imprimir_sent(t):
     'sent_imprimir  : expresion'
+    t[0] = t[1]
+
 def p_asignacion(t):
     'asignacion    : ID DOSPUNTOS IGUAL expresion PUNTOYCOMA'
+    t[0] = Asignacion(t[1], t[4])
 
 def p_definicion(t):
     'declaracion    :  ID tipo DOSPUNTOS IGUAL expresion PUNTOYCOMA'
+    t[0] = ListaDeclaraciones(t[2], [Declaracion(t[1], t[5])])
 
 def p_definicion_2(t):
     'declaracion    :  ID tipo PUNTOYCOMA'
+    t[0] = ListaDeclaraciones(t[2], [Declaracion(t[1], None)])
 
 def p_definicion_3(t):
     'declaracion    :  ID tipo DEFAULT expresion PUNTOYCOMA'
+    t[0] = ListaDeclaraciones(t[2], [Declaracion(t[1], t[4])])
 
 def p_tipo_dato(t):
     '''tipo     : INTEGER
@@ -363,40 +435,67 @@ def p_tipo_dato(t):
                 | FLOAT
                 | BOOLEAN
                 | VOID'''
+    if t[1] == 'integer': t[0] = TIPO_DATO.INT
+    elif t[1] == 'smallint': t[0] = TIPO_DATO.INT
+    elif t[1] == 'bigint': t[0] = TIPO_DATO.INT
+    elif t[1] == 'decimal': t[0] = TIPO_DATO.FLOAT
+    elif t[1] == 'numeric': t[0] = TIPO_DATO.FLOAT
+    elif t[1] == 'real': t[0] = TIPO_DATO.FLOAT
+    elif t[1] == 'void': t[0] = TIPO_DATO.INT
+    elif t[1] == 'char': t[0] = TIPO_DATO.CHAR
+    elif t[1] == 'double': t[0] = TIPO_DATO.DOUBLE
+    elif t[1] == 'precision': t[0] = TIPO_DATO.DOUBLE
+    elif t[1] == 'money': t[0] = TIPO_DATO.DOUBLE
+    elif t[1] == 'float': t[0] = TIPO_DATO.FLOAT
+    elif t[1] == 'boolean': t[0] = TIPO_DATO.BOOLEAN
 
 def p_tipo_dato_cadena(t):
     'tipo     : CHAR PARA ENTERO PARC'
+    t[0] = TIPO_DATO.STRING
 
 def p_tipo_dato_cadena2(t):
     'tipo     : CHARACTER VARYING PARA ENTERO PARC'
+    t[0] = TIPO_DATO.STRING
 
 def p_tipo_dato_cadena3(t):
     'tipo     : VARCHAR PARA ENTERO PARC'
+    t[0] = TIPO_DATO.STRING
 
 def p_tipo_dato_cadena4(t):
     'tipo     : CHARACTER PARA ENTERO PARC'
+    t[0] = TIPO_DATO.STRING
 
 def p_tipo_dato_cadena5(t):
     'tipo     : TEXT'
+    t[0] = TIPO_DATO.STRING
 
 def p_tipo_dato_time(t):
     ' tipo :  TIME'
+    t[0] = TIPO_DATO.STRING
 
 def p_tipo_dato_time2(t):
     ' tipo :  TIMESTAMP'
+    t[0] = TIPO_DATO.STRING
 
 def p_tipo_dato_tim3(t):
     'tipo : DATE'
+    t[0] = TIPO_DATO.STRING
 
 def p_expresion(t):
     '''expresion    : log'''
+    t[0] = t[1]
 
 def p_log(t):
     '''log      : expresion AND expresion
                 | expresion ORR expresion'''
+    if t[2] == '&&':
+        t[0] = ExpresionBinaria(t[1], t[3], OPERADOR.AND)
+    elif t[2] == '||':
+        t[0] = ExpresionBinaria(t[1], t[3], OPERADOR.OR)
 
 def p_log_uni(t):
     '''log      : rel'''
+    t[0] = t[1]
 
 
 def p_rel(t):
@@ -406,6 +505,18 @@ def p_rel(t):
                 | arit MENORIGUAL arit
                 | arit IGUALIGUAL arit
                 | arit NOTIGUAL arit'''
+    if t[2] == '>':
+        t[0] = ExpresionBinaria(t[1], t[3], OPERADOR.MAYOR)
+    elif t[2] == '<':
+        t[0] = ExpresionBinaria(t[1], t[3], OPERADOR.MENOR)
+    elif t[2] == '>=':
+        t[0] = ExpresionBinaria(t[1], t[3], OPERADOR.MAYORIGUAL)
+    elif t[2] == '<=':
+        t[0] = ExpresionBinaria(t[1], t[3], OPERADOR.MENORIGUAL)
+    elif t[2] == '==':
+        t[0] = ExpresionBinaria(t[1], t[3], OPERADOR.IGUAL)
+    elif t[2] == '!=':
+        t[0] = ExpresionBinaria(t[1], t[3], OPERADOR.DIFERENTE)
 
 def p_rel_arit(t):
     '''rel      : arit'''
@@ -422,15 +533,38 @@ def p_arit(t):
                 | arit SHIFTD arit
                 | arit XORB arit
                 | arit ORB arit'''
+    if t[2] == '*':
+        t[0] = ExpresionBinaria(t[1], t[3], OPERADOR.POR)
+    elif t[2] == '/':
+        t[0] = ExpresionBinaria(t[1], t[3], OPERADOR.DIVIDIDO)
+    elif t[2] == '+':
+        t[0] = ExpresionBinaria(t[1], t[3], OPERADOR.MAS)
+    elif t[2] == '-':
+        t[0] = ExpresionBinaria(t[1], t[3], OPERADOR.MENOS)
+    elif t[2] == '%':
+        t[0] = ExpresionBinaria(t[1], t[3], OPERADOR.MOD)
+    elif t[2] == '&':
+        t[0] = ExpresionBinaria(t[1], t[3], OPERADOR.ANDB)
+    elif t[2] == '<<':
+        t[0] = ExpresionBinaria(t[1], t[3], OPERADOR.SHIFTI)
+    elif t[2] == '>>':
+        t[0] = ExpresionBinaria(t[1], t[3], OPERADOR.SHIFTD)
+    elif t[2] == '|':
+        t[0] = ExpresionBinaria(t[1], t[3], OPERADOR.ORB)
+    elif t[2] == '^':
+        t[0] = ExpresionBinaria(t[1], t[3], OPERADOR.XORB)
 
 def p_arit_parentecis(t):
     ''' arit    : PARA expresion PARC'''
+    t[0] = t[2]
 
 def p_arit_ID(t):
     ''' arit    : ID'''
+    t[0] = ExpresionIdentificador(t[1])
 
 def p_arit_cadena(t):
     ''' arit    : CADENA'''
+    t[0] = ExpresionCadena(t[1])
 
 def p_arit_numero(t):
     ''' arit    : ENTERO
@@ -438,11 +572,22 @@ def p_arit_numero(t):
                 | MENOS expresion %prec UMENOS
                 | NOTB expresion
                 | NOT expresion'''
+    if t[1] == '-' :
+        t[0] = ExpresionNegativo(t[2])
+    elif t[1] == '~' :
+        t[0] = ExpresionNOTBIN(t[2])
+    elif t[1] == '!':
+        t[0] = ExpresionNOT(t[2])
+    else:
+        t[0] = ExpresionNumero(t[1])
+
 def p_arit_numero1(t):
     ''' arit    : TRUE'''
+    t[0] = ExpresionBooleana(t[1])
 
 def p_arit_numero2(t):
     ''' arit    : FALSE'''
+    t[0] = ExpresionBooleana(t[1])
 
 # Epsilon
 def p_empty(t):
@@ -452,6 +597,7 @@ def p_empty(t):
 # Errores Sintacticos
 def p_error(t):
     print("Error sintáctico en '%s'" % t.value)
+    listaErroresSintacticos.append(ErrorLexico(t.value, t.lineno, t.lexpos))
 
 
 # Función para realizar analisis
@@ -462,5 +608,4 @@ def parse(input):
     lexer = lex.lex()
     return parser.parse(input)
 
-parse('SELECT hola();')
 
