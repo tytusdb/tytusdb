52254a968798e93e05a0270842f39f455164e419
201602569 <christophersoto97@gmail.com>
2021-01-04 22:53:01 -0600

    [ADD] primitivos bigint,date,char,character,money,numeric,real,smallint,text,time,timestamp.varchar

---
+++ parser/fase2/team17/Traduccion/InterpreteF2/Primitivos/REAL.py
@@ -0,0 +1,30 @@
+from Interprete.NodoAST import NodoArbol
+from Interprete.Tabla_de_simbolos import Tabla_de_simbolos
+from Interprete.Arbol import Arbol
+from Interprete.Valor.Valor import Valor
+from Interprete.Primitivos.TIPO import TIPO
+
+class REAL(NodoArbol):
+
+    def __init__(self, data, line, column):
+        super().__init__(line, column)
+        self.data = data
+
+    def analizar_semanticamente(self, entorno: Tabla_de_simbolos, arbol:Arbol):
+        return 7
+
+    def traducir(self, entorno: Tabla_de_simbolos, arbol:Arbol):
+        temp = arbol.getTemp()
+        arbol.addC3D(temp + " = "  + str(self.data) )
+        return temp
+
+    def execute(self, entorno: Tabla_de_simbolos, arbol:Arbol):
+        value:Valor = Valor(TIPO.ENTERO, self.data)
+        return value
+
+    def getString(self, entorno: Tabla_de_simbolos, arbol:Arbol):
+        return str(self.data)
+
+    def getValueAbstract(self, entorno: Tabla_de_simbolos, arbol:Arbol):
+        value:Valor = Valor(7, self.data)
+        return value
