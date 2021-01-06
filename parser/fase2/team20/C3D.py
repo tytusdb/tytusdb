2533dd1fba542fffdc7995f5bb36b85786fb7d3f
Diego <gomezefdiego@gmail.com>
2021-01-06 14:05:30 -0600

    Merging conflicts

--- parser/fase2/team20/C3D.py
+++ parser/fase2/team20/C3D.py
@@ -5,7 +5,6 @@ from goto import with_goto
 
 @with_goto
 def up():
-<<<<<<< HEAD
 	print(VALIDAREGISTROS('TBPRODUCTO',1))
 	print(VALIDAREGISTROS('TBPRODUCTO',2))
 	print(VALIDAREGISTROS('TBPRODUCTOUP',2))
@@ -99,17 +98,3 @@ def VALIDAREGISTROS(TABLA: str, CANTIDAD: int) ->int:
 	return t27
 
 up()
-=======
-	print(1)
-
-def SALES_TAX(SUBTOTAL: float) ->float:
-	t0=23
-	t1=5
-	t2=t0*t1
-	t3=6
-	t4=2
-	t5=t3/t4
-	t6=t2+t5
-	TOTAL:float=t6
-	print(1)
->>>>>>> a29902789cc622cad6c3a731cde75479ec3aea50
