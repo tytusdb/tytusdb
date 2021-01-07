d8583953b0b59a62828ca926611936dc25547244
Luis <bachejin@gmail.com>
2021-01-07 11:00:05 -0600

    Lista de objetos optimizados, selects de funciones y prints ddl

--- parser/fase2/team24/c3d.py
+++ parser/fase2/team24/c3d.py
@@ -13,19 +13,17 @@ for i in range(100):
 
 def ejecutar(): 
 	n_db = tabla.id_db(NombreDB)
-	NuevoSimbolo = Simbolo(cont,myFuncion,TIPO.FUNCTION,n_db)
+	NuevoSimbolo = Simbolo(cont,'myFuncion',TIPO.FUNCTION,n_db)
 	cont+=1
-	pila[0] = 'INICIO CALIFICACION FASE 2'
-	myFuncion()
-	print(pila[10])
 
+t1 = 'INICIO CALIFICACION FASE 2'
+pila[0] = t1
+myFuncion()
+print(pila[10])
 def myFuncion():
 	texto = pila[0]
 	t0 = texto
 	
 	pila[10] = t0
-
-
-
 	
 ejecutar() 
--- parser/fase2/team24/grammar2.py
+++ parser/fase2/team24/grammar2.py
@@ -2508,7 +2508,9 @@ def p_createproc(t):
     'createproc : CREATE PROCEDURE ID PARA lparamsp PARC LANGUAGE PLPGSQL AS DOLAR DOLAR block PUNTOCOMA DOLAR DOLAR'
     t[0] = createfunc(t[3],t[5],None,t[12])
 
-
+def p_queryf(t):
+    'queryf : SELECT newexp PUNTOCOMA'
+    t[0] = queryf(t[2])
 
 
 def p_error(t):
@@ -2519,7 +2521,7 @@ def p_error(t):
         nuevo_error = CError(linea,columna,descript,'Sintactico')
         insert_error(nuevo_error)
         parser.errok()
-        print(t)
+        #print(t)
     else:
         print("No se pudo recuperar")
     return
