446130c9cdd0d09befe548ed25f48c9f3b1c61a6
Gleimy Polanco <gleypolanco@gmail.com>
2021-01-06 23:32:55 -0600

    Update interfaz.py
    Boton Grafo

--- storage/fase2/team02/interfaz.py
+++ storage/fase2/team02/interfaz.py
@@ -6,6 +6,11 @@ ventana.title('TYTUS DB II FASE')
 imagen = ImageTk.PhotoImage(Image.open(r'C:\Users\rospo\Documents\GitHub\tytus\storage\fase2\team02\fondo.jpg').resize((500, 500)))
 labelimagen = Label(image=imagen)
 
+botonGrafo1 = Button(labelimagen , text = "   Grafo 1   ")
+botonGrafo1.place(x=325, y = 110)
+botonGrafo1.config(background = "#70af85",  relief = "raised", borderwidth=6, fg="white", font=("Helvetica", 12 , "bold") )
+
+
 
 labelimagen.pack()
 ventana.mainloop()
