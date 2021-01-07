cf695b130b8fabc0842081cac98d2fca6e71e019
montherbasir <monther.basir@gmail.com>
2021-01-06 18:37:37 -0600

    [FEAT]: Traduccion de Drop Function y Funciones de SQL
    Se implemento la eliminacion de funciones y procedimientos. Se implemento la traduccion de funciones de SQL.

---
+++ parser/fase2/team29/analizer_pl/C3D/operations/datatype.py
@@ -0,0 +1,60 @@
+from analizer_pl.abstract.expression import Expression
+from analizer_pl.modules.expressions import C3D
+from analizer_pl.C3D.operations import operation
+
+
+class Identifier(Expression):
+    def __init__(self, id, isBlock, row, column) -> None:
+        super().__init__(row, column)
+        self.id = id
+        self.isBlock = isBlock
+
+    def execute(self, environment):
+        if self.isBlock:
+            return C3D("", self.id, self.row, self.column)
+
+        if environment.getVar(self.id):
+            return C3D("", "\""+self.id+"\"", self.row, self.column)
+
+        return C3D("", self.id, self.row, self.column)
+
+
+class BinaryExpression(Expression):
+    def __init__(self, temp, exp1, exp2, operator, isBlock, row, column):
+        super().__init__(row, column)
+        self.temp = temp
+        self.exp1 = exp1
+        self.exp2 = exp2
+        self.operator = operator
+        self.isBlock = isBlock
+
+    def execute(self, environment):
+        if self.isBlock:
+            op = operation.Binary(
+                self.temp, self.exp1, self.exp2, self.operator, self.row, self.column)
+            return op.execute(environment)
+        
+        c3d = self.exp1.execute(environment).temp
+        c3d += self.operator.execute(environment).temp
+        c3d += self.exp2.execute(environment).temp
+        return C3D("", c3d, self.row, self.column)
+    
+
+class UnaryExpression(Expression):
+    def __init__(self, temp, exp, operator, isBlock, row, column):
+        super().__init__(row, column)
+        self.temp = temp
+        self.exp = exp
+        self.operator = operator
+        self.isBlock = isBlock
+    
+    def execute(self, environment):
+        if self.isBlock:
+            op = operation.Unary(
+                self.temp, self.exp, self.operator, self.row, self.column)
+            return op.execute(environment)
+        
+        c3d = self.operator.execute(environment).temp
+        c3d = self.exp.execute(environment).temp
+        return C3D("", c3d, self.row, self.column)
+
