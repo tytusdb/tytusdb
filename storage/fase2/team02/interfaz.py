7f98f186f14379c5ad596b96fe80fae324ee7328
Gleimy Polanco <gleypolanco@gmail.com>
2021-01-06 23:35:19 -0600

    Botones

--- storage/fase2/team02/interfaz.py
+++ storage/fase2/team02/interfaz.py
@@ -11,6 +11,18 @@ botonGrafo1.place(x=325, y = 110)
 botonGrafo1.config(background = "#70af85",  relief = "raised", borderwidth=6, fg="white", font=("Helvetica", 12 , "bold") )
 
 
+botonGrafo2 = Button(labelimagen , text = "   Grafo 2   ")
+botonGrafo2.place(x=325, y = 200)
+botonGrafo2.config(background = "#aa8976",  relief = "raised", borderwidth=6,  fg="white", font=("Helvetica", 12 , "bold") )
+
+botonBlock = Button(labelimagen , text = "Blockchain")
+botonBlock.place(x=205, y = 200)
+botonBlock.config(background = "#70af85",  relief = "raised", borderwidth=6,  fg="white", font=("Helvetica", 12 , "bold") )
+
+
+botonBase = Button(labelimagen , text = " Base D ")
+botonBase.place(x=110, y = 110)
+botonBase.config(background = "#70af85",  relief = "raised", borderwidth=6,  fg="white", font=("Helvetica", 12 , "bold") )
 
 labelimagen.pack()
 ventana.mainloop()
