108897a72d32e53e646cd6ad7b8625b53f45eb4b
Sergio Otzoy <otzoy97@hotmail.com>
2021-01-08 10:12:40 -0600

    endpoint actualizados

---
+++ server/fase2/team05/controller/interpreter.py
@@ -0,0 +1,4 @@
+from Fase1.analizer import interpreter
+
+def exec(query):
+    return interpreter.execution(query)
--- server/fase2/team05/routes/database.py
+++ server/fase2/team05/routes/database.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, Response, jsonify, request
 from flask_cors import CORS
+from controller import interpreter
 
 dbs = Blueprint('dbs', __name__)
 
@@ -7,9 +8,22 @@ CORS(dbs)
 
 @dbs.route('/create/<name>', methods=['GET'])
 def create(name):
-    return {"msg": f'database {name} created'} , 200
+    try:
+        # Construye el query para intentar crear una 
+        # base de datos con el nombre dado
+        query = f'CREATE DATABASE IF NOT EXISTS {name};'
+        result = interpreter.exec(query)
+        return {"result": result, "ok": True} , 200
+    except Exception as e:
+        print(e)
+        return {"ok": False}, 400
 
 @dbs.route('/showall', methods=['GET'])
 def showAll():
-    databases = ["db1", "db2", "db3", "db4"]
-    return {"payload": databases}, 200
+    try:
+        query = f'SHOW DATABASES;'
+        result = interpreter.exec(query)
+        return {"result": result, "ok": True}, 200
+    except Exception as e:
+        print(e)
+        return {"ok": False}, 400
