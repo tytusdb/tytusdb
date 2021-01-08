85cc6c07085b9553b2bc0dc2aefac12b67f37dc2
Juliocotzo <Juliocotzo11@gmail.com>
2021-01-08 12:55:48 -0600

    Fase 2

--- parser/fase2/team15/TytusDB_G15/salida3D.py
+++ parser/fase2/team15/TytusDB_G15/salida3D.py
@@ -27,177 +27,57 @@ def main():
 	inter.procesar_funcionInsert10()
 	inter.procesar_funcionInsert11()
 	inter.procesar_funcionInsert12()
-	inter.procesar_funcionInsert13()	
-	tp0 = 'Crea Funcion'	
-	pos = pos + 1	
-	arr[pos] = 2	
-	goto. myFuncion	
-	label. retorno2
+	inter.procesar_funcionInsert13()
 	print('\nCREATE FUNCTION')	
 	tp1 = 'tbProducto'	
 	tp2 = 8	
 	pos = pos + 1	
-	arr[pos] = 3	
+	arr[pos] = 2	
 	goto. ValidaRegistros	
-	label. retorno3
+	label. retorno2
 	inter.procesar_funcionInsert14()
 	inter.procesar_funcionUpdate15()	
 	tp1 = 'tbProductoUp'	
 	tp2 = 8	
 	pos = pos + 1	
-	arr[pos] = 4	
+	arr[pos] = 3	
 	goto. ValidaRegistros	
-	label. retorno4
+	label. retorno3
 	inter.procesar_funcionInsert16()
 	print('\nCREATE FUNCTION')	
 	pos = pos + 1	
-	arr[pos] = 5	
+	arr[pos] = 4	
 	goto. CALCULOS	
-	label. retorno5
+	label. retorno4
 	inter.procesar_funcionInsert17()
 	inter.procesar_funcionCreateTable18()
 	inter.procesar_funcionCreateIndex19()
 	print('\nCREATE FUNCTION')	
 	pos = pos + 1	
-	arr[pos] = 6	
+	arr[pos] = 5	
 	goto. sp_validainsert	
-	label. retorno6	
+	label. retorno5	
 	tp1 = 'tbbodega'	
 	tp2 = 5	
 	pos = pos + 1	
-	arr[pos] = 7	
+	arr[pos] = 6	
 	goto. ValidaRegistros	
-	label. retorno7
+	label. retorno6
 	inter.procesar_funcionInsert20()
-	inter.procesar_funcionCreateIndex21()
-	inter.procesar_funcionDropIndex22()
-	inter.procesar_funcionCreateIndex23()
 	print('\nCREATE FUNCTION')	
 	pos = pos + 1	
-	arr[pos] = 8	
+	arr[pos] = 7	
 	goto. sp_validaupdate	
-	label. retorno8
-	inter.procesar_funcionDelete24()	
+	label. retorno7
+	inter.procesar_funcionDelete21()	
 	tp1 = 'tbbodega'	
 	tp2 = 4	
 	pos = pos + 1	
-	arr[pos] = 9	
+	arr[pos] = 8	
 	goto. ValidaRegistros	
-	label. retorno9
-	inter.procesar_funcionInsert25()
-	inter.procesar_funcionSelect26()
-	inter.procesar_funcionCreateIndex27()
-	print('\nCREATE FUNCTION')	
-	tp3 = 9	
-	tp4 = 'Bocina Inalambrica'	
-	tp5 = '2021-01-06'	
-	pos = pos + 1	
-	arr[pos] = 10	
-	goto. sp_insertaproducto	
-	label. retorno10	
-	tp3 = 10	
-	tp4 = 'Audifonos con Microfono USB'	
-	tp5 = '2021-01-06'	
-	pos = pos + 1	
-	arr[pos] = 11	
-	goto. sp_insertaproducto	
-	label. retorno11	
-	tp3 = 11	
-	tp4 = 'Bocina Inalambrica'	
-	tp5 = '2021-01-06'	
-	pos = pos + 1	
-	arr[pos] = 12	
-	goto. sp_insertaproducto	
-	label. retorno12	
-	tp3 = 12	
-	tp4 = 'Monitor de 17"'	
-	tp5 = '2021-01-06'	
-	pos = pos + 1	
-	arr[pos] = 13	
-	goto. sp_insertaproducto	
-	label. retorno13
-	print('\nDROP FUNCTION')
-	print('\nNOTICE:  function "myFundsdsadsadasdcion" does not exist, skipping\nDROP FUNCTION')
-	print('\nERROR:  could not find a function named "myFundsdsadsadasdcion"\nSQL state: 42883')	
-	pos = pos + 1	
-	arr[pos] = 14	
-
-	print('\nERROR:  function myFuncion() does not exist\nSQL state: 42883')	
-	label. retorno14
-	print('\nCREATE FUNCTION')	
-	pos = pos + 1	
-	arr[pos] = 15	
-
-	print('\nERROR:  function myFuncion() does not exist\nSQL state: 42883')	
-	label. retorno15	
-	tp3 = 13	
-	tp4 = 'Bocina Inalambrica Sony'	
-	tp5 = '2021-01-06'	
-	pos = pos + 1	
-	arr[pos] = 16	
-	goto. sp_insertaproducto	
-	label. retorno16	
-	tp3 = 14	
-	tp4 = 'Audifonos con Microfono USB Lenovo'	
-	tp5 = '2021-01-06'	
-	pos = pos + 1	
-	arr[pos] = 17	
-	goto. sp_insertaproducto	
-	label. retorno17	
-	tp3 = 15	
-	tp4 = 'Monitor de 21"'	
-	tp5 = '2021-01-06'	
-	pos = pos + 1	
-	arr[pos] = 18	
-	goto. sp_insertaproducto	
-	label. retorno18	
-	tp3 = 16	
-	tp4 = 'Monitor de 17" Lenovo'	
-	tp5 = '2021-01-06'	
-	pos = pos + 1	
-	arr[pos] = 19	
-	goto. sp_insertaproducto	
-	label. retorno19
-	inter.procesar_funcionCreateTable28()
-	print('\nCREATE FUNCTION')
-	print('\nCREATE FUNCTION')
-	print('\nCREATE FUNCTION')	
-	tp9 = 1	
-	tp10 = 'Laptop Lenovo'	
-	tp11 = 'BODEGA CENTRAL'	
-	tp12 = 200	
-	tp13 = 'Laptop Lenovo T420 i7 8GB'	
-	pos = pos + 1	
-	arr[pos] = 20	
-	goto. sp_insertainventario	
-	label. retorno20	
-	tp9 = 2	
-	tp10 = 'Teclado Inalambrico'	
-	tp11 = 'BODEGA CENTRAL'	
-	tp12 = 100	
-	tp13 = 'Teclado Inalambrico Lenovo'	
-	pos = pos + 1	
-	arr[pos] = 21	
-	goto. sp_insertainventario	
-	label. retorno21	
-	tp9 = 3	
-	tp10 = 'Mouse Inalambrico'	
-	tp11 = 'BODEGA ZONA 12'	
-	tp12 = 50	
-	tp13 = ''	
-	pos = pos + 1	
-	arr[pos] = 22	
-	goto. sp_insertainventario	
-	label. retorno22	
-	tp9 = 4	
-	tp10 = 'Laptop HP'	
-	tp11 = 'bodega zona 9'	
-	tp12 = 20	
-	tp13 = 'Laptop HP i5 4GB RAM'	
-	pos = pos + 1	
-	arr[pos] = 23	
-	goto. sp_insertainventario	
-	label. retorno23
+	label. retorno8
+	inter.procesar_funcionInsert22()
+	inter.procesar_funcionSelect23()
 	inter.Reportes()	
 	goto. end	
 	
@@ -212,7 +92,7 @@ def main():
 	if  t2 : goto. L0	
 	goto. L1	
 	label. L0	
-	t0 = 1	
+	t0 = 2	
 	t3 = tp2 == t0	
 	if  t3 : goto. L3	
 	goto. L4	
@@ -242,7 +122,7 @@ def main():
 	if  t6 : goto. L12	
 	goto. L13	
 	label. L12	
-	t0 = 3	
+	t0 = 2	
 	t7 = tp2 == t0	
 	if  t7 : goto. L15	
 	goto. L16	
@@ -261,15 +141,15 @@ def main():
 	t9 = 0	
 	t10 = 0	
 	t11 = 0	
-	t8 = 20	
-	t9 = 1	
-	t10 = 20	
-	t12 = t10 + 3	
+	t8 = 2	
+	t9 = 3	
+	t10 = 4	
+	t12 = t10 + 2	
 	t10 = t12	
-	t11 = 1	
-	t13 = t11 * 5	
-	t11 = t13	
-	t14 = t10 + t11	
+	t11 = 3	
+	t11 = 4	
+	t13 = t10 + t11	
+	t14 = t13 / 2	
 	t10 = t14	
 	t15 = t10 > 1	
 	if  t15 : goto. L18	
@@ -284,50 +164,17 @@ def main():
 	goto. retorno	
 	
 	label. sp_validainsert
-	inter.procesar_funcionInsert29()
-	inter.procesar_funcionInsert30()
-	inter.procesar_funcionInsert31()
-	inter.procesar_funcionInsert32()
-	inter.procesar_funcionInsert33()	
+	inter.procesar_funcionInsert24()
+	inter.procesar_funcionInsert25()
+	inter.procesar_funcionInsert26()
+	inter.procesar_funcionInsert27()
+	inter.procesar_funcionInsert28()	
+	print('INSERT  ')	
 	goto. retorno	
 	
-	label. sp_validaupdate
-	inter.procesar_funcionUpdate34()	
-	goto. retorno	
-	
-	label. sp_insertaproducto
-	inter.procesar_funcionInsert35()	
-	goto. retorno	
-	
-	label. fn_Mensaje	
-	print(tp6 )	
-	goto. retorno	
-	
-	label. fn_retornaproducto	
-	t16 = 0	
-	t16 = 30	
-	print(t16 )	
-	goto. retorno	
-	
-	label. fn_retornabodega	
-	t17 = 0	
-	t17 = 29	
-	print(t17 )	
-	goto. retorno	
-	
-	label. sp_insertainventario	
-	t18 = 0	
-	t19 = 0	
-	t20 = 0	
-	t20 = 0	
-	t21 = t20 == 0	
-	if  t21 : goto. L21	
-	goto. L22	
-	label. L21	
-	t18 = 2	
-	t19 = 2	
-	label. L22	
-	print(tp9 )	
+	label. sp_validaupdate	
+	print('UPDATE  ')
+	inter.procesar_funcionUpdate29()	
 	goto. retorno	
 	
 	label. retorno	
@@ -340,22 +187,7 @@ def main():
 	if posR == 5: goto. retorno5	
 	if posR == 6: goto. retorno6	
 	if posR == 7: goto. retorno7	
-	if posR == 8: goto. retorno8	
-	if posR == 9: goto. retorno9	
-	if posR == 10: goto. retorno10	
-	if posR == 11: goto. retorno11	
-	if posR == 12: goto. retorno12	
-	if posR == 13: goto. retorno13	
-	if posR == 14: goto. retorno14	
-	if posR == 15: goto. retorno15	
-	if posR == 16: goto. retorno16	
-	if posR == 17: goto. retorno17	
-	if posR == 18: goto. retorno18	
-	if posR == 19: goto. retorno19	
-	if posR == 20: goto. retorno20	
-	if posR == 21: goto. retorno21	
-	if posR == 22: goto. retorno22	
-	if posR == 23: goto. retorno23
+	if posR == 8: goto. retorno8
 
 	label .end
 	return
