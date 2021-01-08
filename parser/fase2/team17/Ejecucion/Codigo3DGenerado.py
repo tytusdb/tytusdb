146ade6c0a6c060b50f2fca0de1920b8a93e30b6
Ned <tesco.herrera.edu@gmail.com>
2021-01-08 12:17:11 -0600

    [fix] correcion a gramatica

--- parser/fase2/team17/Ejecucion/Codigo3DGenerado.py
+++ parser/fase2/team17/Ejecucion/Codigo3DGenerado.py
@@ -1,10 +1,33 @@
 
 from Fase1.Sql import Sql
 from goto import with_goto
-heap = None
-def inter():
-    global  heap
-    sql:Sql = Sql()
-    sql.run(heap)
+heap = ''
+def inter() -> str:
+	global heap
+	sql: Sql = Sql()
+	result = str(sql.query(heap))
+	return result
+
+@with_goto
+def principal():
+	t0 = 0
+	t1 = 'hola ' + t0 + ' como estas'
+	print(str(t1))
+	t2 = 5
+	t3 = 6
+	if t2 > t3:
+		goto .L0
+	else:
+		goto .L1
+	label .L0
+	t4 = 'hola'
+	print(str(t4))
+	goto .L2
+	label .L1
+	t5 = 'adios'
+	print(str(t5))
+	label .L2
+
+
 if __name__ == '__main__':
 	principal()
