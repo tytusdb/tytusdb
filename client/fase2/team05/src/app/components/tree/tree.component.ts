d0dde6795ad4cf0295662b65762bc5c2a036df1d
bmoisesg <bmoisesg@gmail.com>
2021-01-08 16:48:47 -0600

    update code recibiendo datos de servidr}or

--- client/fase2/team05/src/app/components/tree/tree.component.ts
+++ client/fase2/team05/src/app/components/tree/tree.component.ts
@@ -33,48 +33,13 @@ export class TreeComponent implements OnInit {
   }
 
   refresh() {
-    alert("este es un texto")
-    this.consumir_servicio()
-    
-    alert("def showDatabases()")                  //funcion a la que se invocara en siguiente fase
-    let item_db = document.getElementById("db")
-    item_db.innerHTML = ""                        //limpiando los datos almacenados 
-    this.json = prompt("recibiendo lista de bases de datos...", "[basedatos1,basedatos2, basedatos3]");
-    
-    //let array = this.parser(this.json)            //array que contiene las bases de datos
-    let array= this.array_database_service
-
-    console.log("data base : ", array)
-    document.getElementById("num_db").innerText = String(array.length)  //  actualizando el numero de base de datos en el navegador
-    var contenedor=""
-    for (let i = 0; i < array.length; i++) {
-
-      item_db.innerHTML += 
-        '<li> <span class="caret"> <i class="fa fa-database"></i> ' + array[i] +' </span>'+
-        '<ul class="animacion">'+
-        '<li><span class="caret"><i class="fa fa-folder-o"></i> Tablas [<a id ="t_' +array[i] +'">0</a>]</span>'+
-        '<ul class="animacion" id="'+array[i] +'"></ul>'+
-        '</li></ul></li>'
-
-     /* let item_tabla = document.getElementById(array[i])
-      alert("def showtablees(" + array[i] + ")")        //funcion a la que se invocara en la siguiente fase
-      this.json = prompt("recibiendo tablas para la base de datos '" + array[i]+"'", "[ciudad,cliente,factura,proveedor]");
-      let respuesta = this.parser(this.json)                                          // array que contiene las tablas 
-      document.getElementById('t_' + array[i]).innerText=  String(respuesta.length)   // actualiza el numero de tabla en cada base de datos
-      /*
-      for (let i = 0; i < respuesta.length; i++) {
-        item_tabla.innerHTML += '<li><i class="fa fa-table"></i> ' + respuesta[i] + '</li>'
-      }*/
-    }
-    this.funcion1()
+    this.consumir_servicio()  
   }
 
   consumir_servicio(){
-    var tmp: any[]
+    var tmp: []
     this.ServiceTreeService.getData_treedatabase().subscribe(
       res=> {
-
-
 
 
        this.data=(res)
@@ -96,6 +61,7 @@ export class TreeComponent implements OnInit {
               console.log(`${k}:`);
               it(v, tabSize + 1);
             } else {
+              //console.log(`${'\t'.repeat(tabSize)}${k}`);
               hola.push(`${'\t'.repeat(tabSize)}${k}`)
             }
           }
@@ -109,7 +75,7 @@ export class TreeComponent implements OnInit {
 
         console.log("***********************************")
         console.log(hola)
-        this.array_database_service= hola
+        this.array_database_service=hola
         /*
         for (const key in tmp) {
           if (Object.prototype.hasOwnProperty.call(tmp, key)) {
@@ -119,7 +85,37 @@ export class TreeComponent implements OnInit {
         }*/
 
 
-
+        alert("def showDatabases()")                  //funcion a la que se invocara en siguiente fase
+        let item_db = document.getElementById("db")
+        item_db.innerHTML = ""                        //limpiando los datos almacenados 
+        this.json = prompt("recibiendo lista de bases de datos...", "[basedatos1,basedatos2, basedatos3]");
+        
+        //let array = this.parser(this.json)            //array que contiene las bases de datos
+        let array= this.array_database_service
+    
+        console.log("data base : ", array)
+        document.getElementById("num_db").innerText = String(array.length)  //  actualizando el numero de base de datos en el navegador
+        var contenedor=""
+        for (let i = 0; i < array.length; i++) {
+    
+          item_db.innerHTML += 
+            '<li> <span class="caret"> <i class="fa fa-database"></i> ' + array[i] +' </span>'+
+            '<ul class="animacion">'+
+            '<li><span class="caret"><i class="fa fa-folder-o"></i> Tablas [<a id ="t_' +array[i] +'">0</a>]</span>'+
+            '<ul class="animacion" id="'+array[i] +'"></ul>'+
+            '</li></ul></li>'
+    
+         /* let item_tabla = document.getElementById(array[i])
+          alert("def showtablees(" + array[i] + ")")        //funcion a la que se invocara en la siguiente fase
+          this.json = prompt("recibiendo tablas para la base de datos '" + array[i]+"'", "[ciudad,cliente,factura,proveedor]");
+          let respuesta = this.parser(this.json)                                          // array que contiene las tablas 
+          document.getElementById('t_' + array[i]).innerText=  String(respuesta.length)   // actualiza el numero de tabla en cada base de datos
+          /*
+          for (let i = 0; i < respuesta.length; i++) {
+            item_tabla.innerHTML += '<li><i class="fa fa-table"></i> ' + respuesta[i] + '</li>'
+          }*/
+        }
+        this.funcion1()
 
 
       },err=>{
@@ -128,15 +124,6 @@ export class TreeComponent implements OnInit {
     );
    
   }
-  parser(data) {
-    var array = []
-    data = data.replace('[', "")
-    data = data.replace(']', "")
-    var list_data = data.split(',')
-    for (let i = 0; i < list_data.length; i++) {
-      array.push(list_data[i])
-    }
-    return array
-  }
+ 
 
 }
