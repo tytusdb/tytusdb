a35ea18add56ebae234f5d9257d5968617696c6c
Abelarde <datosmoviles52@gmail.com>
2021-01-08 00:08:37 -0600

    arreglando unos errores

--- client/fase2/team05/src/app/app.settings.ts
+++ client/fase2/team05/src/app/app.settings.ts
@@ -1,3 +1,3 @@
 export class AppSettings {
-    public static API_ENDPOINT = 'http://localhost:5000/'
+    public static API_ENDPOINT = 'http://localhost:5000/';
 }
--- client/fase2/team05/src/app/service/table/table.service.ts
+++ client/fase2/team05/src/app/service/table/table.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import {AppSettings} from '../../app.settings'
+import { HttpClient } from '@angular/common/http';
+import {AppSettings} from '../../app.settings';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +8,8 @@ import {AppSettings} from '../../app.settings'
 export class TableService {
   private URL = AppSettings.API_ENDPOINT + 'table/';
   constructor(private http: HttpClient) { }
+  // tslint:disable-next-line:typedef
   create(name: string, database: string) {
-    return this.http.get<any>(this.URL + `create/${database}/${name}`, {observe: 'response'})
+    return this.http.get<any>(this.URL + `create/${database}/${name}`, {observe: 'response'});
   }
 }
