ba4a2a665089538c73d5ca5261b47ef26d83556d
Brayan Brito <2196007320101@ingenieria.usac.edu.gt>
2021-01-08 14:50:14 -0600

    plsql y optimización de código

--- parser/fase2/team04/Tytus/Instrucciones/FunctionMathematical/Abs.py
+++ parser/fase2/team04/Tytus/Instrucciones/FunctionMathematical/Abs.py
@@ -10,7 +10,7 @@ class Abs(Instruccion):
 
     def ejecutar(self, tabla, arbol):
         super().ejecutar(tabla,arbol)
-        resultado = self.valor.ejecutar(tabla,arbol)
+        """ resultado = self.valor.ejecutar(tabla,arbol)
         if isinstance(resultado, Excepcion):
             return resultado
         if self.valor.tipo.tipo != Tipo_Dato.SMALLINT and self.valor.tipo.tipo != Tipo_Dato.INTEGER and self.valor.tipo.tipo != Tipo_Dato.BIGINT and self.valor.tipo.tipo != Tipo_Dato.DECIMAL and self.valor.tipo.tipo != Tipo_Dato.NUMERIC and self.valor.tipo.tipo != Tipo_Dato.REAL and self.valor.tipo.tipo != Tipo_Dato.DOUBLE_PRECISION:
@@ -23,4 +23,24 @@ class Abs(Instruccion):
             return int(abs(resultado))
         else:
             self.tipo = Tipo(Tipo_Dato.NUMERIC)
-            return abs(resultado)
+            return abs(resultado) """
+        return int(abs(self.valor))
+        
+    def getCodigo(self, tabla, arbol):
+        result = self.valor.getCodigo(tabla, arbol)        
+        value_list = []
+        
+        value_list.append(result['dir'])
+        value_list.append(f"\"{self.strGram}\"")
+        value_list.append(self.linea)
+        value_list.append(self.columna)
+        
+        native_result = arbol.getExpressionCode(value_list, 'abs')
+        
+        codigo = result['codigo']
+        codigo += native_result['codigo']
+        
+        return {'codigo': codigo, 'dir': native_result['dir']}
+    
+    def toString(self):
+        return f"ABS({self.valor})"
--- parser/fase2/team04/Tytus/Instrucciones/plsql/clase_if.py
+++ parser/fase2/team04/Tytus/Instrucciones/plsql/clase_if.py
@@ -10,9 +10,24 @@ class clase_if(Instruccion): #block y _else_block son listas
         self.columna = columna
                       
     def ejecutar(self, tabla, arbol):
-        
        pass
 
 
     def getCodigo(self, tabla, arbol):
-        pass
+        label_true = arbol.getLabel()
+        label_false = arbol.getLabel()
+        label_salida = arbol.getLabel()
+        
+        result = self.expresion.getCodigo(tabla, arbol)
+        
+        codigo = result['codigo']
+        codigo += f"\tif({result['dir']}): goto .{label_true}\n"
+        codigo += f"\tgoto .{label_false}\n"
+        codigo += f"\tlabel .{label_true}\n"
+        codigo += self.block.getCodigo(tabla, arbol)
+        codigo += f"\tgoto .{label_salida}\n"
+        codigo += f"\tlabel .{label_false}\n"
+        codigo += self._else_block.getCodigo(tabla, arbol) if self._else_block else ""
+        codigo += f"\tlabel .{label_salida}\n"
+        
+        return codigo
