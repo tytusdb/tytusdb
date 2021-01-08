b1bdd7fd85b61c54839b79361ad3ea3959439728
Sergio Otzoy <otzoy97@hotmail.com>
2021-01-07 22:59:34 -0600

    agregado espacio de ejecución de querys

--- server/fase2/team05/routes/index.py
+++ server/fase2/team05/routes/index.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from .table import tables
 from .database import dbs
-# importar el archivo query
+from .query import qry
 
 def init_routes(app):
     app.register_blueprint(tables, url_prefix='/table')
     app.register_blueprint(dbs, url_prefix='/db')
-    # crear la ruta para query , con ruta '/query/
+    app.register_blueprint(qry, url_prefix='/query')
