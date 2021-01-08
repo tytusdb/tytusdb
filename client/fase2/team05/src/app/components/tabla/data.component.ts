bed49bcb6b596344da657a06ba0dc565b4fc0532
Abelarde <datosmoviles52@gmail.com>
2021-01-08 12:47:06 -0600

    mostrando tabla con datos

--- client/fase2/team05/src/app/components/tabla/data.component.ts
+++ client/fase2/team05/src/app/components/tabla/data.component.ts
@@ -2,62 +2,74 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatPaginatorModule} from '@angular/material/paginator';
+import { TableDataService } from 'src/app/service/tableData/table-data.service';
 
+const text = '{\n' +
+  '    "ok": true,\n' +
+  '    "result": {\n' +
+  '        "lexical": [],\n' +
+  '        "messages": [\n' +
+  '            "Select ejecutado con exito."\n' +
+  '        ],\n' +
+  '        "postgres": [],\n' +
+  '        "querys": [\n' +
+  '            [\n' +
+  '                [\n' +
+  '                    "productos.nombre",\n' +
+  '                    "categoria.nombre"\n' +
+  '                ],\n' +
+  '                [\n' +
+  '                    [\n' +
+  '                        "Escoba",\n' +
+  '                        "Abarrotes"\n' +
+  '                    ],\n' +
+  '                    [\n' +
+  '                        "Set de cubiertos",\n' +
+  '                        "Abarrotes"\n' +
+  '                    ]\n' +
+  '                ]\n' +
+  '            ]\n' +
+  '        ],\n' +
+  '        "semantic": [],\n' +
+  '        "symbols": [],\n' +
+  '        "syntax": []\n' +
+  '    }\n' +
+  '}\n' +
+  '\n' +
+  '  \n';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-];
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styleUrls: ['./data.component.css']
 })
+
 export class DataComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  data: PeriodicElement[] = ELEMENT_DATA;
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-  columnsToDisplay: string[] = this.displayedColumns.slice();
+
+  constructor(private servicio: TableDataService) { }
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
 
-  // tslint:disable-next-line:typedef
-  addColumn() {
-    const randomColumn = Math.floor(Math.random() * this.displayedColumns.length);
-    this.columnsToDisplay.push(this.displayedColumns[randomColumn]);
-  }
-
-  // tslint:disable-next-line:typedef
-  removeColumn() {
-    if (this.columnsToDisplay.length) {
-      this.columnsToDisplay.pop();
-    }
-  }
-
-
-  constructor() { }
+  headers: string[]; // = ['ID', 'Name', 'Age', 'Gender', 'Country']
+  rows: string[]; // = [[**,**],[**,**]]
 
   ngOnInit(): void {
+  }
+
+  // tslint:disable-next-line:typedef
+  fillData(){
+    // this.servicio.create(this.query).subscribe((response) => {
+    //   const body = response.body;
+    //   const msg = body.msg;
+    // });
+
+    const obj = JSON.parse(text);
+    const arreglo = obj.result.querys;
+    this.headers = arreglo[0][0];
+    this.rows = arreglo[0][1];
+
   }
 
 }
