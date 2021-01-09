a5af11c95f24e0b1eec70d9d7cea55e24dfc0b70
Madelyn Zuseth Perez Rosales <34404458+MadelynZuseth@users.noreply.github.com>
2021-01-09 09:22:07 -0600

    Update storageManager.py
    grafoDF

--- storage/fase2/team05/storageManager.py
+++ storage/fase2/team05/storageManager.py
@@ -1085,6 +1085,66 @@ def alterDatabaseEncoding(database: str, encoding: str) -> int:
             return 2
     except Exception:
         return 1
+    
+def graphDF(database: str, table: str):
+    try:
+        s = open('graph2.dot', 'w')
+        s.write('digraph G{\n')
+        s.write('rankdir = \"LR\" \n')
+        s.write('node[shape=record]\n')
+        retorno = showTables(database)
+        tabla = []
+        for y in range(0, len(retorno)):
+            if(retorno[y]==table):
+                tabla=retorno[y]   
+
+        NoColumnas = len(tabla)
+        numero = 0
+        s.write("" + tabla + "[label=\"")
+    
+        while(numero < NoColumnas-1):
+            if(numero == NoColumnas - 2):
+                s.write("<f" + str(numero) + ">" + str(numero) + "\", group = 0];")
+            else:
+                s.write("<f" + str(numero) + ">" + str(numero) + "|")
+            numero = numero + 1
+    
+        primaria = llavePrimarias(database, table)
+        print("primaria")
+        print(primaria)
+
+        foranea = []
+    
+        boolean = False
+        numero = 0
+        while(numero < NoColumnas -1):
+            for x in range(0, len(primaria)):
+                if(numero == primaria[x]):
+                    s.write("" + str(numero) + "[Label =\"" + str(numero) + "\", group = 1];" )
+                    boolean = True
+            if (boolean == False):
+                s.write(str(numero) + "[Label =\"" + str(numero) + "\", group = 2];" )
+                foranea.append(numero)
+            else:
+                boolean = False
+            numero = numero + 1
+        print("foranea")
+        print(foranea)
+
+        for x in range(0, len(primaria)):
+            for y in range(0, len(foranea)):
+                s.write("" + str(primaria[x]) + "->" + str(foranea[y]) + ";")
+        s.write('}')
+        s.close()
+
+        path=os.getcwd()
+        print('path'+path)
+        
+        os.system('dot -Tpdf graph2.dot -o graph2.pdf')
+        os.system('graph2.pdf')
+
+    except Exception:
+        return None   
 
 def encrypt(backup: str, password: str) -> str:
     return cripto.encrypt(backup, password)
