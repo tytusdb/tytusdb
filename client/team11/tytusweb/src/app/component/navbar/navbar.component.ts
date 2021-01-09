2ad3367de18bc79d4bf9f4af1bb4f5cafb9652c2
RomaelP <30426188+RomaelP@users.noreply.github.com>
2021-01-08 22:28:14 -0600

    Funcionalidades

--- client/team11/tytusweb/src/app/component/navbar/navbar.component.ts
+++ client/team11/tytusweb/src/app/component/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PruebaService } from 'src/app/service/prueba.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private pruebaService: PruebaService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  public new() {
+    Swal.fire({
+      title: 'هل تريد الاستمرار؟',
+      icon: 'question',
+      iconHtml: '؟',
+      confirmButtonText: 'نعم',
+      cancelButtonText: 'لا',
+      showCancelButton: true,
+      showCloseButton: true
+    })
+  }
+
+  public aboutUs(){
+
+    this.pruebaService.aboutUs().subscribe(
+      res=>{
+        console.log();
+        Swal.fire(
+          'GRUPO 5',
+          //@ts-ignore
+          `${res.COORDINADOR} // ${res.INTEGRANTES[0]} // ${res.INTEGRANTES[1]}`        
+        )
+      },
+      err => console.error(err)
+    );
+  }
+
+  
+
+
+ 
 }
